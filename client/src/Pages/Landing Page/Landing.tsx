9505948bbc7bb58650f7b3dacd457c08605aa4ec
Jonatahn Bajada <jbajada@ryerson.ca>
2023-03-07 22:43:50 -0500

    updated nav settings

--- client/src/Pages/Landing Page/Landing.tsx
+++ client/src/Pages/Landing Page/Landing.tsx
@@ -3,28 +3,18 @@ import Team from '../Landing Page/components/Team/Team';
 import Clients from '../Landing Page/components/Clients/Clients';
 import '../../styles/globalStyles.css';
 import styles from './Landing.module.css';
-import { scroller } from 'react-scroll';
 import Footer from '../../components/Footer/Footer';
 import Nav2 from '../../components/Nav/Nav2';
-import Offer from './components/Offer/Offer';
-import About from './components/About/About';
-const Landing = (props: any) => {
-	const scrollToSection = () => {
-		scroller.scrollTo('services', {
-			duration: 800,
-			delay: 0,
-			smooth: 'easeInOutQuart'
-		});
-	};
 
+const Landing = (props: any) => {
 	return (
 		<main className={styles.mainContainer}>
 			<Nav2 />
-			<Header scrollRef={scrollToSection} />
-			<Offer />
+			<Header />
+			{/* <Offer />
 			<About />
 			<Team />
-			<Clients />
+			<Clients /> */}
 			<Footer />
 		</main>
 	);
--- client/src/Pages/Landing Page/components/Header/Header.tsx
+++ client/src/Pages/Landing Page/components/Header/Header.tsx
@@ -33,24 +33,22 @@ const Header = (props: any) => {
 				<motion.div initial={{ opacity: 0 }} animate="show" variants={list} className={styles.textContainer}>
 					<div className={styles.topText}>
 						<motion.h1 initial={{ y: 100, opacity: 0 }} variants={item} className={styles.text}>
-							We
-						</motion.h1>
-						<motion.h1 initial={{ y: 100, opacity: 0 }} variants={item} className={styles.text}>
-							Design
+							We Design
 						</motion.h1>
 					</div>
 					<div className={styles.topText}>
 						<motion.h1 initial={{ y: 100, opacity: 0 }} variants={item} className={styles.text}>
-							Custom
-						</motion.h1>
-						<motion.h1 initial={{ y: 100, opacity: 0 }} variants={item} className={styles.text}>
-							Software
+							Custom Software
 						</motion.h1>
 					</div>
 				</motion.div>
-				<div className={styles.cubeIMG}>
-					<img src={cube} width={100} />
-				</div>
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1, transition: { delay: 0.5 } }}
+					className={styles.cubeIMG}
+				>
+					<img src={cube} className={styles.cubeSize} />
+				</motion.div>
 				{/* <motion.div
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1, transition: { delay: 0.8 } }}
@@ -58,10 +56,14 @@ const Header = (props: any) => {
 				>
 					<Cube />
 				</motion.div> */}
-				<div className={styles.buttonGroup}>
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1, transition: { delay: 0.5 } }}
+					className={styles.buttonGroup}
+				>
 					<button className={styles.button}>Contact</button>
 					<button className={styles.button}>Learn More</button>
-				</div>
+				</motion.div>
 			</div>
 
 			<motion.div
