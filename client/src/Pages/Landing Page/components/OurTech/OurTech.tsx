07c02bf4ab777d689bf9a95aba54019faedd21a0
Jonatahn Bajada <jbajada@ryerson.ca>
2023-03-08 18:54:29 -0500

    added infinite scroll marq

--- client/src/Pages/Landing Page/components/OurTech/OurTech.tsx
+++ client/src/Pages/Landing Page/components/OurTech/OurTech.tsx
@@ -3,8 +3,42 @@ import { SiTypescript, SiPostgresql, SiAdobexd } from 'react-icons/si';
 import { DiJava, DiJavascript1 } from 'react-icons/di';
 import styles from './OurTech.module.scss';
 import { motion } from 'framer-motion';
+import { useRef, useEffect } from 'react';
 
 const OurTech = () => {
+	const marqueeRef = useRef<HTMLDivElement>(null);
+	let requestRef = useRef<number>(0);
+  
+	useEffect(() => {
+	  const marqueeElement = marqueeRef.current;
+	  if (!marqueeElement) return;
+  
+	  const cloneNode = marqueeElement.cloneNode(true) as HTMLDivElement;
+	  marqueeElement.parentNode?.appendChild(cloneNode);
+  
+	  let currentX = 0;
+	  let speed = 0.5;
+  
+	  const move = (timestamp: number) => {
+		if (marqueeElement.offsetWidth === 0) return;
+  
+		currentX += speed;
+		if (currentX >= marqueeElement.offsetWidth) {
+		  currentX = 0;
+		}
+		marqueeElement.style.transform = `translateX(-${currentX}px)`;
+		cloneNode.style.transform = `translateX(-${currentX}px)`;
+  
+		requestRef.current = requestAnimationFrame(move);
+	  };
+  
+	  requestRef.current = requestAnimationFrame(move);
+  
+	  return () => {
+		cancelAnimationFrame(requestRef.current);
+	  };
+	}, []);
+
 	const marqueeVariants = {
 		animate: {
 			x: [ -240, -1035 ],
@@ -22,7 +56,7 @@ const OurTech = () => {
 		<main className={styles.container}>
 			{/* <h2 className={styles.title}>Technologies</h2> */}
 
-			<motion.div initial="hidden" variants={marqueeVariants} animate="animate" className={styles.iconContainer}>
+			<motion.div ref={marqueeRef} className={styles.iconContainer}>
 				<DiJavascript1 />
 				<SiTypescript />
 				<SiPostgresql />
@@ -60,6 +94,13 @@ const OurTech = () => {
 				<SiAdobexd />
 				<FaWordpress />
 			</motion.div>
+			{/* <div className="marquee-container">
+				<div className="marquee" ref={marqueeRef}>
+					<img src="/logo1.png" alt="Logo 1" />
+					<img src="/logo2.png" alt="Logo 2" />
+					<img src="/logo3.png" alt="Logo 3" />
+				</div>
+			</div> */}
 		</main>
 	);
 };
