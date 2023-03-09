aad8625d08271e38124f27875ebcb0132d40c1ea
Jonatahn Bajada <jbajada@ryerson.ca>
2023-03-09 15:20:06 -0500

    update ham menu

--- client/src/Pages/Landing Page/components/Offer/Offer.tsx
+++ client/src/Pages/Landing Page/components/Offer/Offer.tsx
@@ -3,7 +3,7 @@ import styles from './Offer.module.scss';
 import { motion } from 'framer-motion';
 import { BsArrowRightCircle } from 'react-icons/bs';
 import software from '../../../../assets/Home/software.png';
-import uxdesign from '../../../../assets/Home/uxdesign.png';
+import uxdesign from '../../../../assets/Home/uxdesign.jpg';
 import code from '../../../../assets/Home/code.jpg';
 import web from '../../../../assets/Home/web.jpg';
 
