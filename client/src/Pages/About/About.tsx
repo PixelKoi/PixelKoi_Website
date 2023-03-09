ac379c506a435e83005f249b97ed9200597a4ade
Jonatahn Bajada <jbajada@ryerson.ca>
2023-03-09 14:23:51 -0500

    pics

--- client/src/Pages/About/About.tsx
+++ client/src/Pages/About/About.tsx
@@ -8,7 +8,8 @@ import garo from '../../assets/images/about_page/garo.jpeg';
 import jonny from '../../assets/images/about_page/jonny3.png';
 import Team from './Team/Team';
 import Footer from '../../components/Footer/Footer';
-
+import about from '../../assets/About/creative.jpg';
+import dream from '../../assets/About/dream.jpg';
 const About = () => {
 	return (
 		<div className={styles.wrapper}>
@@ -62,28 +63,29 @@ const About = () => {
 						</p>
 					</div>
 					<div className={styles.imgContainer}>
-						<img loading="lazy" src={paper} alt="" className={styles.imgContainer} />
-						<img loading="lazy" src={paper2} alt="" className={styles.imgContainer} />
+						<img loading="lazy" src={dream} alt="" className={styles.imgContainer} />
+						<img loading="lazy" src={about} alt="" className={styles.imgContainer} />
 					</div>
 					<div className={styles.description}>
 						<p>
 							We are a{' '}
 							<span className={styles.hightlight}>
 								<b>curious</b>
-							</span>{' '} and
+							</span>{' '}
+							and
 							<span className={styles.hightlight}>
 								<b> creative</b>
-							</span>{' '} team
-							of skilled professionals, each with our own unique niches of expertise, coming together to
-							deliver exceptional results through shared passion, curiosity and
+							</span>{' '}
+							team of skilled professionals, each with our own unique niches of expertise, coming together
+							to deliver exceptional results through shared passion, curiosity and
 							<span className={styles.hightlight}> meticulous</span> planning, we work closely with our
-							clients during each stage of the project to comprehend their distinctive requirements and provide
-							excellent user experiences for their users. Through our expertise and inventive solutions,
-							we carve a path that leads to a product that exceeds expectations.
-							Our proven track record and unwavering commitment to improving our designs and ingenuity is
-							a testament to our dedication to deliver excellence in every project we choose to undertake.
-							When you work with us, you can be confident that you are partnering with a team of true
-							professionals who will help you achieve your goals and take your brand to the next level.
+							clients during each stage of the project to comprehend their distinctive requirements and
+							provide excellent user experiences for their users. Through our expertise and inventive
+							solutions, we carve a path that leads to a product that exceeds expectations. Our proven
+							track record and unwavering commitment to improving our designs and ingenuity is a testament
+							to our dedication to deliver excellence in every project we choose to undertake. When you
+							work with us, you can be confident that you are partnering with a team of true professionals
+							who will help you achieve your goals and take your brand to the next level.
 						</p>
 					</div>
 				</div>
@@ -96,14 +98,13 @@ const About = () => {
 						<img loading="lazy" src={hipsters} alt="" className={styles.imgContainer} />
 						<p>
 							As a team of experienced professional software engineers, we bring our passion for
-							problem-solving and ingenuity to each project we decide to undertake.
-							With a keen attention to detail, and insatiable curiosity, we work closely with our select
-							clients throughout the entire development process to gain a deep understanding of their
-							unique needs.
-							Our goal is to deliver a seamless user experience, creating unique and memorable brands for
-							our clients that not only meets but exceeds users and clients expectations.
-							We take great pride in our ability to tackle
-							complex challenges and provide customized solutions that drive our clients' success.
+							problem-solving and ingenuity to each project we decide to undertake. With a keen attention
+							to detail, and insatiable curiosity, we work closely with our select clients throughout the
+							entire development process to gain a deep understanding of their unique needs. Our goal is
+							to deliver a seamless user experience, creating unique and memorable brands for our clients
+							that not only meets but exceeds users and clients expectations. We take great pride in our
+							ability to tackle complex challenges and provide customized solutions that drive our
+							clients' success.
 						</p>
 					</div>
 				</div>
