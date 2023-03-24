2eb2c35dfd61d5153f7f2c9f7fb91323c6d77454
Jonatahn Bajada <jbajada@ryerson.ca>
2023-03-24 13:09:15 -0400

    updatd framer

--- client/src/components/Footer/Footer2.tsx
+++ client/src/components/Footer/Footer2.tsx
@@ -7,51 +7,40 @@ import { AiFillLinkedin } from 'react-icons/ai';
 const Footer2 = () => {
 	let currentYear = new Date().getFullYear();
 	return (
-		<motion.div
-			initial={{ opacity: 0 }}
-			whileInView={{ opacity: 1, transition: { duration: 0.5 } }}
-			viewport={{ once: true }}
-			className={styles.wrapper}
-		>
+		<motion.div className={styles.wrapper}>
 			<div className={styles.container}>
-				<div className={styles.consultation}>
+				<motion.div
+					initial={{ y: 20, opacity: 0 }}
+					whileInView={{ y: 0, opacity: 1, transition: { duration: 1 } }}
+					viewport={{ once: true }}
+					className={styles.consultation}
+				>
 					<h4>GOT A PROJECT IN MIND?</h4>
-					<motion.div
-						initial={{ y: 20, opacity: 0 }}
-						whileInView={{ y: 0, opacity: 1, transition: { duration: 1 } }}
-						viewport={{ once: true }}
-					>
+					<motion.div>
 						<a href="mailto:jonathanbajada@pixelkoi.com?subject=Book%20a%20consultation">
 							Book a consultation
 						</a>
 					</motion.div>
-				</div>
-				<div className={styles.linksContainer}>
+				</motion.div>
+				<motion.div
+					initial={{ y: 20, opacity: 0 }}
+					whileInView={{ y: 0, opacity: 1, transition: { duration: 1 } }}
+					viewport={{ once: true }}
+					className={styles.linksContainer}
+				>
 					<h4>INFORMATION</h4>
 					<div className={styles.linksWrapper} id={styles.links}>
-						<motion.div
-							initial={{ y: 20, opacity: 0 }}
-							whileInView={{ y: 0, opacity: 1, transition: { duration: 1 } }}
-							viewport={{ once: true }}
-						>
+						<motion.div>
 							<Link to="/about" title="Description page for Pixel Koi Company">
 								About
 							</Link>
 						</motion.div>
-						<motion.div
-							initial={{ y: 20, opacity: 0 }}
-							whileInView={{ y: 0, opacity: 1, transition: { duration: 1.1 } }}
-							viewport={{ once: true }}
-						>
+						<motion.div>
 							<Link to="/#services" title="Shows services Pixel Koi Company Provides">
 								Services
 							</Link>
 						</motion.div>
-						<motion.div
-							initial={{ y: 20, opacity: 0 }}
-							whileInView={{ y: 0, opacity: 1, transition: { duration: 1.2 } }}
-							viewport={{ once: true }}
-						>
+						<motion.div>
 							<Link
 								to="/contact"
 								title="Takes customer to the contact form allowing communication with Pixel Koi Company"
@@ -60,15 +49,16 @@ const Footer2 = () => {
 							</Link>
 						</motion.div>
 					</div>
-				</div>
-				<div className={styles.linksContainer}>
+				</motion.div>
+				<motion.div
+					initial={{ y: 20, opacity: 0 }}
+					whileInView={{ y: 0, opacity: 1, transition: { duration: 1 } }}
+					viewport={{ once: true }}
+					className={styles.linksContainer}
+				>
 					<h4>SOCIALS</h4>
 					<div className={styles.linksWrapper} id={styles.socials}>
-						<motion.div
-							initial={{ y: 20, opacity: 0 }}
-							whileInView={{ y: 0, opacity: 1, transition: { duration: 1 } }}
-							viewport={{ once: true }}
-						>
+						<motion.div>
 							<a
 								href="https://www.linkedin.com/company/pixelkoi/"
 								target="_blank"
@@ -78,13 +68,18 @@ const Footer2 = () => {
 							</a>
 						</motion.div>
 					</div>
-				</div>
+				</motion.div>
 			</div>
-			<div className={styles.copyrightContainer}>
+			<motion.div
+				initial={{ opacity: 0 }}
+				whileInView={{ opacity: 1, transition: { duration: 1 } }}
+				viewport={{ once: true }}
+				className={styles.copyrightContainer}
+			>
 				<motion.p className={styles.footerCopyright}>
 					Copyright © {currentYear} Pixel Koi - All rights reserved.
 				</motion.p>
-			</div>
+			</motion.div>
 		</motion.div>
 	);
 };
