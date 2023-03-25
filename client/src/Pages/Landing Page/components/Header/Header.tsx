117870b12bc40e7c112924dc94b8dbdf02732157
Garabed96 <garonazarian09@gmail.com>
2023-03-25 17:20:34 +0700

    Center Modal

--- client/src/Pages/Landing Page/components/Header/Header.tsx
+++ client/src/Pages/Landing Page/components/Header/Header.tsx
@@ -18,59 +18,39 @@ const Header = (props: any) => {
 	return (
 		<div className={styles.header} id="header" style={{backgroundImage: `url(${headerImg})`}}>
 			<div className={styles.container}>
-				<motion.div initial={{ opacity: 0 }} animate="show" variants={list} className={styles.textContainer}>
-					<div className={styles.topText}>
-						<motion.h1 initial={{ y: 100, opacity: 0 }} variants={item} className={styles.text}>
-							We Create
-						</motion.h1>
-					</div>
-					<div className={styles.topText}>
-						<motion.h1 initial={{ y: 100, opacity: 0 }} variants={item} className={styles.text}>
-							Software Solutions
-						</motion.h1>
-					</div>
-				</motion.div>
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1, transition: { delay: 0.7 } }}
-					className={styles.cubeIMG}
-				>
-					<img alt="cube" src={cube} className={styles.cubeSize} />
-				</motion.div>
-				{/* <motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1, transition: { delay: 0.8 } }}
-					className={styles.cube}
-					style={{ marginBottom: '5rem' }}
-				>
-					<Cube />
-				</motion.div> */}
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1, transition: { delay: 0.9 } }}
-					className={styles.buttonGroup}
-				>
-					<Link
-						to={'/contact'}
-						title="Takes customer to the contact form allowing communication with Pixel Koi Company"
+				<div className={styles.modalContainer}>
+					<motion.div initial={{ opacity: 0 }} animate="show" variants={list} className={styles.textContainer}>
+						<div className={styles.headerText}>
+							<motion.h1 initial={{ y: 100, opacity: 0 }} variants={item} className={styles.text}>
+								We Create
+							</motion.h1>
+						</div>
+						<div className={styles.headerText}>
+							<motion.h1 initial={{ y: 100, opacity: 0 }} variants={item} className={styles.text}>
+								Software Solutions
+							</motion.h1>
+						</div>
+					</motion.div>
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1, transition: { delay: 0.9 } }}
+						className={styles.buttonGroup}
 					>
-						<button className={styles.button} id={styles.buttonContact}>
-							Contact
-						</button>
-					</Link>
-					<Link to={'#services'}>
-						<button className={styles.button}>Learn More</button>
-					</Link>
-				</motion.div>
-			</div>
+						<Link
+							to={'/contact'}
+							title="Takes customer to the contact form allowing communication with Pixel Koi Company"
+						>
+							<button className={styles.button} id={styles.buttonContact}>
+								Contact
+							</button>
+						</Link>
+						<Link to={'#services'}>
+							<button className={styles.button}>Learn More</button>
+						</Link>
+					</motion.div>
+				</div>
 
-			{/* <motion.div
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1, transition: { delay: 0.5 } }}
-				className={styles.techMarq}
-			>
-				<OurTech />
-			</motion.div> */}
+			</div>
 		</div>
 	);
 };
