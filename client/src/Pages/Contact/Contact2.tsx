d8aade0f51dcd080990d22838b3687226266e1d4
Jonatahn Bajada <jbajada@ryerson.ca>
2023-03-28 18:02:56 -0400

    made contact page responsive

--- client/src/Pages/Contact/Contact2.tsx
+++ client/src/Pages/Contact/Contact2.tsx
@@ -161,6 +161,7 @@ export const Contact = () => {
 			<div className={styles.headerWrapper}>
 				<div className={styles.header}>
 					<motion.h1
+						className={styles.headerText}
 						initial={{ opacity: 0, y: 80 }}
 						animate={{ opacity: 1, y: 0, transition: { delay: 0.4, duration: 0.4 } }}
 					>
