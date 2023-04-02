b2f530c4a7244aa58dc1b4d3957911a062dc087c
Garabed96 <garonazarian09@gmail.com>
2023-04-03 02:49:55 +0700

    copyright

--- server/src/app.tsx
+++ server/src/app.tsx
@@ -1,71 +1,68 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-const cors = require('cors');
-const nodemailer = require('nodemailer');
-const path = require('path');
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const path = require("path");
 
 app.use(express.json());
 
 app.use(
-	cors({
-		origin: '*'
-	})
+  cors({
+    origin: "*",
+  })
 );
 
 const port = 8000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.post('/send-email', (req, res) => {
-	const { name, company, email, phone } = req.body;
+app.post("/send-email", (req, res) => {
+  const { name, company, email, phone } = req.body;
 
-	const transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: 465,
-		secure: true,
-		auth: {
-			user: 'gmoneydoespython@gmail.com',
-			pass: 'qbqjjywxcdtigbzu'
-		}
-	});
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "gmoneydoespython@gmail.com",
+      pass: "qbqjjywxcdtigbzu",
+    },
+  });
 
-	const mailOptions = {
-		from: email,
-		to: 'garonazarian09@gmail.com',
-		subject: 'Contact Form Submission',
-		html: `
+  const mailOptions = {
+    from: email,
+    to: "garonazarian09@gmail.com",
+    subject: "Contact Form Submission",
+    html: `
       <h2>Contact Form Submission</h2>
       <p><strong>Name:</strong> ${name}</p>
       <p><strong>Email:</strong> ${email}</p>
       <p><strong>Company:</strong> ${company}</p>
       <p><strong>Phone Number:</strong> ${phone}</p>
-    `
-	};
+    `,
+  };
 
-	transporter.sendMail(mailOptions, (error, info) => {
-		if (error) {
-			console.error(error);
-			res.status(500).send('Internal server error');
-		} else {
-<<<<<<< HEAD
-			console.log(`Email sent: ${info.response}`);
-			res.status(200).send('Email sent successfully');
-=======
-			console.log(`Email sent: ${info.response.json}`);
-			res.status(200).json({message: "Email sent successfully"});
->>>>>>> 2b0f71e7e7edac65a7355b6442ae4d13160d8c6d
-		}
-	});
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Internal server error");
+    } else {
+      console.log(`Email sent: ${info.response}`);
+      res.status(200).send("Email sent successfully");
+      console.log(`Email sent: ${info.response.json}`);
+      res.status(200).json({ message: "Email sent successfully" });
+    }
+  });
 });
 
 app.listen(port, () => {
-	return console.log(`Server running on port ${port}`);
+  return console.log(`Server running on port ${port}`);
 });
