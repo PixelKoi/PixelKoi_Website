9b0df70dffee0e7732b32f83485a952edf594398
Garabed96 <garonazarian09@gmail.com>
2023-04-03 16:51:14 +0700

    preparing to implement react-spinners

--- client/src/Pages/Landing Page/Landing.tsx
+++ client/src/Pages/Landing Page/Landing.tsx
@@ -1,33 +1,34 @@
-import Header from './components/Header/Header';
-import Clients from '../Landing Page/components/Clients/Clients';
-import '../../styles/globalStyles.css';
-import styles from './Landing.module.css';
+import Header from "./components/Header/Header";
+import Clients from "../Landing Page/components/Clients/Clients";
+import "../../styles/globalStyles.css";
+import styles from "./Landing.module.css";
+import { GridLoader } from "react-spinners";
 
-import Nav from '../../components/Nav/Nav';
-import Offer from './components/Offer/Offer';
-import Global from './components/Global/Global';
-import OurTech from './components/OurTech/OurTech';
-import Footer from '../../components/Footer/Main/Footer';
-import NavGroup from '../../components/Nav/NavGroup';
+import Nav from "../../components/Nav/Nav";
+import Offer from "./components/Offer/Offer";
+import Global from "./components/Global/Global";
+import OurTech from "./components/OurTech/OurTech";
+import Footer from "../../components/Footer/Main/Footer";
+import NavGroup from "../../components/Nav/NavGroup";
 
 const Landing = (props: any) => {
-	return (
-		<main className={styles.mainContainer}>
-			{' '}
-			<NavGroup />
-			<Header />
-			<OurTech />
-			<Offer />
-			<Global />
-			{/*
+  return (
+    <main className={styles.mainContainer}>
+      {" "}
+      <NavGroup />
+      <Header />
+      <OurTech />
+      <Offer />
+      <Global />
+      {/*
 			<About />
 			<Team />
 			<Clients /> */}
-			<Clients />
-			<OurTech />
-			<Footer />
-		</main>
-	);
+      <Clients />
+      <OurTech />
+      <Footer />
+    </main>
+  );
 };
 
 export default Landing;
