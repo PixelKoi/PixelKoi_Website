346acfa5712dba5102dc6ece926a9761d36c3772
Garabed96 <garonazarian09@gmail.com>
2023-04-04 19:39:23 +0700

    header component added background-image to innerDiv

--- client/src/Pages/Landing Page/components/Header/Header.tsx
+++ client/src/Pages/Landing Page/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 // import './Header.css';
 import React, { useEffect, useRef, useState, useCallback } from "react";
-// import { Blurhash } from "react-blurhash";
+import { encode } from "blurhash";
 import "../../../../styles/globalStyles.css";
 import styles from "./Header.module.scss";
 import OurTech from "../OurTech/OurTech";
@@ -14,64 +14,67 @@ import headerImg from "../../../../assets/Home/box.jpg";
 import ParticlesBackground from "../../../../components/Particles/ParticlesBackground";
 import Particles from "react-tsparticles";
 
+const blurhash = "LKO2?U%2Tw=w]~RBVZRi};RPxuwH";
+
 const Header = (props: any) => {
   const list = { show: { opacity: 1, transition: { staggerChildren: 0.09 } } };
   const item = { show: { y: 0, opacity: 1, transition: { duration: 0.5 } } };
   const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   return (
-    <div
-      className={styles.header}
-      id="header"
-      style={{ backgroundImage: `url(${headerImg})` }}
-    >
+    <div className={styles.header} id="header">
       {/*<Blurhash hash={blurHash} width={50} height={50} />*/}
-      <div></div>
-      {/* <ParticlesBackground /> */}
-      <div className={styles.container}>
-        <div className={styles.modalContainer}>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate="show"
-            variants={list}
-            className={styles.textContainer}
-          >
-            <div className={styles.headerText}>
-              <motion.h1
-                initial={{ y: 100, opacity: 0 }}
-                variants={item}
-                className={styles.text}
-              >
-                We Create
-              </motion.h1>
-            </div>
-            <div className={styles.headerText}>
-              <motion.h1
-                initial={{ y: 100, opacity: 0 }}
-                variants={item}
-                className={styles.text}
-              >
-                Software Solutions
-              </motion.h1>
-            </div>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { delay: 0.9 } }}
-            className={styles.buttonGroup}
-          >
-            <Link
-              to={"/contact"}
-              title="Takes customer to the contact form allowing communication with Pixel Koi Company"
+      <div
+        className={styles.headerBackgroundImg}
+        style={{ backgroundImage: `url(${headerImg})` }}
+      >
+        {/* <ParticlesBackground /> */}
+        <div className={styles.container}>
+          <div className={styles.modalContainer}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate="show"
+              variants={list}
+              className={styles.textContainer}
             >
-              <button className={styles.button} id={styles.buttonContact}>
-                Contact
-              </button>
-            </Link>
-            <Link to={"#services"}>
-              <button className={styles.button}>Learn More</button>
-            </Link>
-          </motion.div>
+              <div className={styles.headerText}>
+                <motion.h1
+                  initial={{ y: 100, opacity: 0 }}
+                  variants={item}
+                  className={styles.text}
+                >
+                  We Create
+                </motion.h1>
+              </div>
+              <div className={styles.headerText}>
+                <motion.h1
+                  initial={{ y: 100, opacity: 0 }}
+                  variants={item}
+                  className={styles.text}
+                >
+                  Software Solutions
+                </motion.h1>
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, transition: { delay: 0.9 } }}
+              className={styles.buttonGroup}
+            >
+              <Link
+                to={"/contact"}
+                title="Takes customer to the contact form allowing communication with Pixel Koi Company"
+              >
+                <button className={styles.button} id={styles.buttonContact}>
+                  Contact
+                </button>
+              </Link>
+              <Link to={"#services"}>
+                <button className={styles.button}>Learn More</button>
+              </Link>
+            </motion.div>
+          </div>
         </div>
       </div>
     </div>
