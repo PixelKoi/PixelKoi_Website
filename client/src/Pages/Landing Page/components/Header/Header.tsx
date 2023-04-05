3fa5c400230f55a21ea4030bdf2ca8682ef4bd20
Garabed96 <garonazarian09@gmail.com>
2023-04-05 17:40:39 +0700

    BlurHashEncoder implementation

--- client/src/Pages/Landing Page/components/Header/Header.tsx
+++ client/src/Pages/Landing Page/components/Header/Header.tsx
@@ -41,43 +41,43 @@ const Header = (props: any) => {
     return encode(imageData.data, imageData.width, imageData.height, 4, 4);
   };
   // TODO: Create encoder component that takes list of images and creates hashObjects
-  // useEffect(() => {
-  //   const loadImage = async (src: any) => {
-  //     const img = new Image();
-  //     img.src = src;
-  //     await img.decode();
-  //     const canvas = document.createElement("canvas");
-  //     const ctx: any = canvas.getContext("2d");
-  //     canvas.width = img.width;
-  //     canvas.height = img.height;
-  //     ctx.drawImage(img, 0, 0);
-  //     const imageData = ctx.getImageData(0, 0, img.width, img.height);
-  //     const blurhash = encode(
-  //       imageData.data,
-  //       imageData.width,
-  //       imageData.height,
-  //       4,
-  //       4
-  //     );
-  //     setBlurhash(blurhash);
-  //     console.log(blurhash);
-  //     console.log(src);
-  //     // Creating image url and respective blurhash objects
-  //     const hashObjects = { url: src, blurHash: blurhash };
-  //     fetch("/api/images", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ images: hashObjects }),
-  //     })
-  //       .then((resp) => resp.json())
-  //       .then((data) => console.log(data.images))
-  //       .catch((error) => console.error(error));
-  //   };
-  //
-  //   loadImage(headerImg);
-  // }, []);
+  useEffect(() => {
+    const loadImage = async (src: any) => {
+      const img = new Image();
+      img.src = src;
+      await img.decode();
+      const canvas = document.createElement("canvas");
+      const ctx: any = canvas.getContext("2d");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+      const imageData = ctx.getImageData(0, 0, img.width, img.height);
+      const blurhash = encode(
+        imageData.data,
+        imageData.width,
+        imageData.height,
+        4,
+        4
+      );
+      setBlurhash(blurhash);
+      console.log(blurhash);
+      console.log(src);
+      // Creating image url and respective blurhash objects
+      const hashObjects = { url: src, blurHash: blurhash };
+      fetch("/api/images", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ images: hashObjects }),
+      })
+        .then((resp) => resp.json())
+        .then((data) => console.log(data.images))
+        .catch((error) => console.error(error));
+    };
+
+    loadImage(headerImg);
+  }, []);
   // const blurHash = await encodeImageToBlurhash(headerImg);
   // Test FETCH
   const testHash = { url: "src", blurHash: "blurhash" };
--- client/src/components/BlurHashEncoder/BlurHashEncoder.tsx
+++ client/src/components/BlurHashEncoder/BlurHashEncoder.tsx
@@ -7,11 +7,60 @@ import Header from "../../Pages/Landing Page/components/Header/Header";
 // hashObject eg; const hashObjects = { url: src, blurHash: blurhash };
 const BlurHashEncoder = (props: any) => {
   const [blurhash, setBlurhash] = useState("");
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
+
+  const loadImage = async (src: string): Promise<HTMLImageElement> =>
+    new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = (...args) => reject(args);
+      img.src = src;
+    });
+  const getImageData = (image: HTMLImageElement): ImageData => {
+    const canvas = document.createElement("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height;
+    const context = canvas.getContext("2d");
+    context?.drawImage(image, 0, 0);
+    return context!.getImageData(0, 0, image.width, image.height);
+  };
+  const encodeImageToBlurhash = async (imageUrl: string): Promise<string> => {
+    const image = await loadImage(imageUrl);
+    const imageData = getImageData(image);
+    return encode(imageData.data, imageData.width, imageData.height, 4, 4);
+  };
+
+  const hashImage = async (src: any) => {
+    const img = new Image();
+    img.src = src;
+    await img.decode();
+    const canvas = document.createElement("canvas");
+    const ctx: any = canvas.getContext("2d");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+    const imageData = ctx.getImageData(0, 0, img.width, img.height);
+    const blurhash = encode(
+      imageData.data,
+      imageData.width,
+      imageData.height,
+      4,
+      4
+    );
+    setBlurhash(blurhash);
+    console.log(blurhash);
+    console.log(src);
+    // Creating image url and respective blurhash objects
+    const hashObjects = { url: src, blurHash: blurhash };
+    const hashPostOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ images: hashObjects }),
+    };
+    fetch("http://localhost:8000/api/images", hashPostOptions)
+      .then((resp) => resp.json())
+      .then((data) => console.log(data.images))
+      .catch((error) => console.error(error));
+  };
 };
 
 export default BlurHashEncoder;
