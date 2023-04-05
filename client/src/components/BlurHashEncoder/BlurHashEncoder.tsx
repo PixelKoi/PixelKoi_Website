3dd6f9a693a2ed6633ec6726e39b868d5c0cb770
Garabed96 <garonazarian09@gmail.com>
2023-04-05 18:19:50 +0700

    imageUrls type annotation, loop through image array for image hashing

--- client/src/components/BlurHashEncoder/BlurHashEncoder.tsx
+++ client/src/components/BlurHashEncoder/BlurHashEncoder.tsx
@@ -4,14 +4,21 @@ import { Blurhash } from "react-blurhash";
 
 // All Image url objects currently in use on our website
 // { name: "image1", url: "https://example.com/image1.jpg" }
-const imageUrls = [
+interface ImageUrl {
+  name: string;
+  url: string;
+}
+const imageUrls: ImageUrl[] = [
   { name: "headerImg", url: "../../assets/Home/box.jpg" },
-  { name: "headerImg", url: "../../assets/Home/box.jpg" },
+  { name: "laptop", url: "../../assets/Home/code.webp" },
+  { name: "tablet", url: "../../assets/Home/uxdesign.webp" },
+  { name: "imac", url: "../../assets/Home/web.webp" },
 ];
 
 // Goes through an array of image locations, creates hashObjects
 // hashObject eg; const hashObjects = { url: src, blurHash: blurhash };
-const BlurHashEncoder = (props: { images: Array<string> }) => {
+// const BlurHashEncoder = (props: { images: Array<string> }) => {
+const BlurHashEncoder = (props: any) => {
   const [blurhash, setBlurhash] = useState("");
 
   /* loads an image from given source URL and returns a promise that resolves to the loaded image
@@ -41,38 +48,42 @@ const BlurHashEncoder = (props: { images: Array<string> }) => {
     return encode(imageData.data, imageData.width, imageData.height, 4, 4);
   };
 
-  const hashImage = async (src: any) => {
-    const img = new Image();
-    img.src = src;
-    await img.decode();
-    const canvas = document.createElement("canvas");
-    const ctx: any = canvas.getContext("2d");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
-    const imageData = ctx.getImageData(0, 0, img.width, img.height);
-    const blurhash = encode(
-      imageData.data,
-      imageData.width,
-      imageData.height,
-      4,
-      4
-    );
-    setBlurhash(blurhash);
-    console.log(blurhash);
-    console.log(src);
-    // Creating image url and respective blurhash objects
-    const hashObjects = { url: src, blurHash: blurhash };
-    const hashPostOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ images: hashObjects }),
-    };
-    fetch("http://localhost:8000/api/images", hashPostOptions)
-      .then((resp) => resp.json())
-      .then((data) => console.log(data.images))
-      .catch((error) => console.error(error));
+  const hashImages = async (imageUrls: any) => {
+    const blurhashes = {};
+    for (const { name, url } of imageUrls) {
+      const img = new Image();
+      img.src = url;
+      await img.decode();
+      const canvas = document.createElement("canvas");
+      const ctx: any = canvas.getContext("2d");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+      const imageData = ctx.getImageData(0, 0, img.width, img.height);
+      const blurhash = encode(
+        imageData.data,
+        imageData.width,
+        imageData.height,
+        4,
+        4
+      );
+      console.log(blurhash);
+      console.log(url);
+      blurhashes[name] = blurhash;
+      const hashPostOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ images: { url, blurHash: blurhash } }),
+      };
+      fetch("http://localhost:8000/api/images", hashPostOptions)
+        .then((resp) => resp.json())
+        .then((data) => console.log(data.images))
+        .catch((error) => console.error(error));
+    }
+    console.log(blurhashes); // You can return this object if you want to use it elsewhere
   };
+
+  hashImages(imageUrls);
 };
 
 export default BlurHashEncoder;
