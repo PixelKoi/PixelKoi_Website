3f1ca8d6c0efe8e46b650b8c1ad7a09fcb7e7860
Garabed96 <garonazarian09@gmail.com>
2023-04-06 22:39:08 +0700

    blurHash header ternary operator

--- client/src/Pages/Landing Page/components/Header/Header.tsx
+++ client/src/Pages/Landing Page/components/Header/Header.tsx
@@ -13,13 +13,26 @@ const hash = "U14o1hx^0QIptPR+E4%059Rk^g%J0$t3^NIY";
 const Header = () => {
   const list = { show: { opacity: 1, transition: { staggerChildren: 0.09 } } };
   const item = { show: { y: 0, opacity: 1, transition: { duration: 0.5 } } };
+  const [loaded, setLoaded] = React.useState(false);
+
+  const handleImageLoad = React.useCallback(() => {
+    setLoaded(true);
+  }, []);
 
   return (
     <div className={styles.header} id="header">
-      <img
-        className={styles.headerBackgroundImg}
-        style={{ backgroundImage: `url(${headerImg})` }}
-      />
+      {loaded ? (
+        <img
+          src={headerImg}
+          alt="header"
+          onLoad={handleImageLoad}
+          className={styles.headerBackgroundImg}
+        />
+      ) : (
+        <Blurhash hash={hash} width={32} height={32} punch={1} />
+      )}
+
+      <img style={{ backgroundImage: `url(${headerImg})` }} />
       <div className={styles.container}>
         <div className={styles.modalContainer}>
           <motion.div
