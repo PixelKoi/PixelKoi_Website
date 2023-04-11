bb9cfc56838d07d1e2978b5df12afc2faace8153
Garabed96 <garonazarian09@gmail.com>
2023-04-11 19:49:10 +0700

    Offer.tsx image hash complete with state management

--- client/src/Pages/Landing Page/components/Offer/Offer.tsx
+++ client/src/Pages/Landing Page/components/Offer/Offer.tsx
@@ -122,14 +122,29 @@ const Offer = () => {
         </div>
 
         <div className={styles.uxGroup}>
-          <motion.img
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { duration: 1 } }}
-            viewport={{ once: true }}
-            src={uxdesign}
-            alt="Ipad image with website wireframe"
-            className={styles.cardIMG}
-          />
+          <div>
+            <div style={{ display: loaded ? "none" : "inline" }}>
+              <Blurhash
+                hash={tablet}
+                width="100%"
+                height="100%"
+                resolutionX={64}
+                resolutionY={64}
+                punch={1}
+                className={styles.headerBackgroundImg}
+              />
+            </div>
+            <motion.img
+              onLoad={() => setLoaded(true)}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, transition: { duration: 1 } }}
+              viewport={{ once: true }}
+              src={uxdesign}
+              alt="Ipad image with website wireframe"
+              className={styles.cardIMG}
+            />
+          </div>
+
           <motion.div
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1, transition: { duration: 1 } }}
@@ -190,14 +205,28 @@ const Offer = () => {
               <p style={{ margin: "auto 0", color: "#fff" }}>Learn More</p>
             </Link>
           </motion.div>
-          <motion.img
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { duration: 1 } }}
-            viewport={{ once: true }}
-            src={web}
-            alt="Showing the development of a website with a macbook pro and imac in the background"
-            className={styles.cardIMG}
-          />
+          <div>
+            <div style={{ display: loaded ? "none" : "inline" }}>
+              <Blurhash
+                hash={imac}
+                width="100%"
+                height="100%"
+                resolutionX={64}
+                resolutionY={64}
+                punch={1}
+                className={styles.headerBackgroundImg}
+              />
+            </div>
+            <motion.img
+              onLoad={() => setLoaded(true)}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, transition: { duration: 1 } }}
+              viewport={{ once: true }}
+              src={web}
+              alt="Showing the development of a website with a macbook pro and imac in the background"
+              className={styles.cardIMG}
+            />
+          </div>
         </div>
       </div>
     </div>
