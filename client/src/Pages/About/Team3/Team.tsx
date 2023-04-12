70fde055d6366d2f21e9ae86e537ba940d01fd69
Jonatahn Bajada <jbajada@ryerson.ca>
2023-04-12 12:55:26 -0400

    updated mobile carousel

--- client/src/Pages/About/Team3/Team.tsx
+++ client/src/Pages/About/Team3/Team.tsx
@@ -28,16 +28,28 @@ const Team = () => {
 
 	return (
 		<div className={styles.wrapper}>
-			<div className={styles.carouselContainer}>
-				<BsFillArrowLeftCircleFill className={styles.left} size={35} onClick={handlePrevClick} />
-				<div className={styles.carousel}>
-					<Card
-						image={images[currentIndex].src}
-						name={images[currentIndex].name}
-						description={images[currentIndex].description}
-					/>
+			<h3 className={styles.teamTitle}>
+				<b>Our Team</b>
+			</h3>
+			<p className={styles.cta}>
+				At our core, we are a team of designers, developers, strategists, and problem-solvers who share a
+				collective passion for leveraging the power of technologies, creativity and imagination in turn creating
+				enthralling digital experiences that make a positive difference in people's lives. Whether we're
+				brainstorming ideas or putting them into action, we work together towards a common goal of crafting
+				impactful solutions that resonate with our clients and their users.
+			</p>
+			<div className={styles.carouselWrapper}>
+				<div className={styles.carouselContainer}>
+					<BsFillArrowLeftCircleFill className={styles.left} size={35} onClick={handlePrevClick} />
+					<div className={styles.carousel}>
+						<Card
+							image={images[currentIndex].src}
+							name={images[currentIndex].name}
+							description={images[currentIndex].description}
+						/>
+					</div>
+					<BsFillArrowRightCircleFill className={styles.right} size={35} onClick={handleNextClick} />
 				</div>
-				<BsFillArrowRightCircleFill className={styles.right} size={35} onClick={handleNextClick} />
 			</div>
 		</div>
 	);
