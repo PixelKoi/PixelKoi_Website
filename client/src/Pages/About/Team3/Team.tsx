f150d815032a549cfa1214dda60461d14abfd489
Jonatahn Bajada <jbajada@ryerson.ca>
2023-04-12 12:49:19 -0400

    added carasoul for mobile

--- client/src/Pages/About/Team3/Team.tsx
+++ client/src/Pages/About/Team3/Team.tsx
@@ -7,19 +7,37 @@ import { BsFillArrowLeftCircleFill } from 'react-icons/bs';
 import { BsFillArrowRightCircleFill } from 'react-icons/bs';
 import jonny from '../../../assets/images/slider/jonny.webp';
 import garo from '../../../assets/images/slider/garo.webp';
+import robothappy from '../../../assets/images/slider/robothappy.webp';
+
+const images = [
+	{ src: jonny, alt: 'Jonathan Bajada', name: 'Jonathan Bajada', description: 'Full-Stack Developer/UX Director' },
+	{ src: garo, alt: 'Garo Bajada', name: 'Garo Bajada', description: 'Full-Stack Developer/Cloud Engineer' },
+	{ src: robothappy, alt: 'Jonathan Bajada', name: 'Jonathan Bajada', description: 'Full-Stack Developer' }
+];
 
 const Team = () => {
+	const [ currentIndex, setCurrentIndex ] = useState(0);
+
+	const handlePrevClick = () => {
+		setCurrentIndex(currentIndex === 0 ? images.length - 1 : currentIndex - 1);
+	};
+
+	const handleNextClick = () => {
+		setCurrentIndex(currentIndex === images.length - 1 ? 0 : currentIndex + 1);
+	};
+
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.carouselContainer}>
-				<BsFillArrowLeftCircleFill className={styles.left} size={35} onClick={() => {}} />
+				<BsFillArrowLeftCircleFill className={styles.left} size={35} onClick={handlePrevClick} />
 				<div className={styles.carousel}>
-					<Card image={jonny} name={'Jonathan Bajada'} />
-					<Card image={garo} name={'Garo Bajada'} />
-					<Card image={jonny} name={'Jonathan Bajada'} />
-					<Card image={garo} name={'Garo Bajada'} />
+					<Card
+						image={images[currentIndex].src}
+						name={images[currentIndex].name}
+						description={images[currentIndex].description}
+					/>
 				</div>
-				<BsFillArrowRightCircleFill className={styles.right} size={35} onClick={() => {}} />
+				<BsFillArrowRightCircleFill className={styles.right} size={35} onClick={handleNextClick} />
 			</div>
 		</div>
 	);
