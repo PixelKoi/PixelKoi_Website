f1fadeb152572067224c9c2ab7df63ba31fea199
Jonatahn Bajada <jbajada@ryerson.ca>
2023-04-17 14:19:10 -0400

    updated flavicon and opening parapgrah

--- client/src/Pages/About/About.tsx
+++ client/src/Pages/About/About.tsx
@@ -1,154 +1,132 @@
-import styles from "./About.module.scss";
-import NavGroup from "../../components/Nav/NavGroup";
-import Team2 from "./Team3/Team";
-import Footer from "../../components/Footer/Main/Footer";
-import creative from "./../../assets/About/creative.webp";
-import dream from "../../assets/About/dream.webp";
-import story from "../../assets/About/story.webp";
-import { HashContext } from "../../components/BlurHashEncoder/BlurHashDecoder";
-import { motion } from "framer-motion";
-import React, { useContext, useEffect, useState } from "react";
-import BlurHashImages from "../Landing Page/components/Offer/components/BlurHashImages";
+import styles from './About.module.scss';
+import NavGroup from '../../components/Nav/NavGroup';
+import Team2 from './Team3/Team';
+import Footer from '../../components/Footer/Main/Footer';
+import creative from '../../assets/About/creative.png';
+import dream from '../../assets/About/dream.webp';
+import story from '../../assets/About/story.webp';
+import { HashContext } from '../../components/BlurHashEncoder/BlurHashDecoder';
+import { motion } from 'framer-motion';
+import React, { useContext, useEffect, useState } from 'react';
+import BlurHashImages from '../Landing Page/components/Offer/components/BlurHashImages';
 
 interface ImageType {
-  [name: string]: {
-    url: string;
-    hash: string;
-  };
+	[name: string]: {
+		url: string;
+		hash: string;
+	};
 }
 
 const About = () => {
-  const imageStyles = {
-    position: "relative",
-    width: "100%",
-    height: "100%",
-    maxWidth: "580px",
-    maxHeight: "380px",
-    minHeight: "330px",
-    overflowX: "hidden",
-    overflowY: "hidden",
-    margin: "auto",
-  };
+	const imageStyles = {
+		position: 'relative',
+		width: '100%',
+		height: '100%',
+		maxWidth: '580px',
+		maxHeight: '380px',
+		minHeight: '330px',
+		overflowX: 'hidden',
+		overflowY: 'hidden',
+		margin: 'auto'
+	};
 
-  const [shouldRenderDream, setShouldRenderDream] = useState(false);
+	const [ shouldRenderDream, setShouldRenderDream ] = useState(false);
 
-  const hashData = useContext<ImageType>(HashContext);
-  console.log("About Page Hash Data: ", hashData);
-  const creativeHash = hashData["creative"].hash;
-  const dreamHash = hashData["dream"].hash;
-  const storyHash = hashData["story"].hash;
+	const hashData = useContext<ImageType>(HashContext);
+	console.log('About Page Hash Data: ', hashData);
+	const creativeHash = hashData['creative'].hash;
+	const dreamHash = hashData['dream'].hash;
+	const storyHash = hashData['story'].hash;
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 768) {
-        setShouldRenderDream(true);
-      } else {
-        setShouldRenderDream(false);
-      }
-    };
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth >= 768) {
+				setShouldRenderDream(true);
+			} else {
+				setShouldRenderDream(false);
+			}
+		};
 
-    handleResize(); // initial call to handleResize
+		handleResize(); // initial call to handleResize
 
-    window.addEventListener("resize", handleResize);
+		window.addEventListener('resize', handleResize);
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
 
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1, transition: { duration: 1 } }}
-      viewport={{ once: true }}
-      className={styles.wrapper}
-    >
-      <NavGroup />
-      <div className={styles.container}>
-        <div className={styles.section}>
-          <div className={styles.headerText}>
-            <h1>
-              <i>Our Agency</i>
-            </h1>
-            <p style={{ fontSize: "20px" }}>
-              Pixel Koi is an innovative software development agency dedicated
-              to crafting impactful experiences through Website and Mobile
-              applications
-            </p>
-          </div>
-          <div className={styles.imgContainer}>
-            {shouldRenderDream && (
-              <BlurHashImages
-                src={dream}
-                hashCode={dreamHash}
-                style={imageStyles}
-              />
-            )}
-            <BlurHashImages
-              src={creative}
-              style={imageStyles}
-              hashCode={creativeHash}
-            />
-          </div>
-          <div className={styles.description}>
-            <p>
-              We are a{" "}
-              <span className={styles.hightlight}>
-                <b>curious</b>
-              </span>{" "}
-              and
-              <span className={styles.hightlight}>
-                <b> creative</b>
-              </span>{" "}
-              team of skilled professionals, each with our own unique niches of
-              expertise, coming together to deliver exceptional results through
-              shared passion, curiosity and
-              <span className={styles.hightlight}> meticulous</span> planning,
-              we work closely with our clients during each stage of the project
-              to comprehend their distinctive requirements and provide excellent
-              user experiences for their users. Through our expertise and
-              inventive solutions, we carve a path that leads to a product that
-              exceeds expectations. Our proven track record and unwavering
-              commitment to improving our designs and ingenuity is a testament
-              to our dedication to deliver excellence in every project we choose
-              to undertake. When you work with us, you can be confident that you
-              are partnering with a team of true professionals who will help you
-              achieve your goals and take your brand to the next level.
-            </p>
-          </div>
-        </div>
+	return (
+		<motion.div
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1, transition: { duration: 1 } }}
+			viewport={{ once: true }}
+			className={styles.wrapper}
+		>
+			<NavGroup />
+			<div className={styles.container}>
+				<div className={styles.section}>
+					<div className={styles.headerText}>
+						<h1>
+							<i>Our Agency</i>
+						</h1>
+						<p style={{ fontSize: '20px' }}>
+							Pixel Koi is an innovative software development agency dedicated to crafting impactful
+							experiences through Website and Mobile applications
+						</p>
+					</div>
+					<div className={styles.imgContainer}>
+						{shouldRenderDream && <BlurHashImages src={dream} hashCode={dreamHash} style={imageStyles} />}
+						<BlurHashImages src={creative} style={imageStyles} hashCode={creativeHash} />
+					</div>
+					<div className={styles.description}>
+						<p>
+							We are a{' '}
+							<span className={styles.hightlight}>
+								<b>curious</b>
+							</span>{' '}
+							and
+							<span className={styles.hightlight}>
+								<b> creative</b>
+							</span>{' '}
+							team of skilled professionals, each with our own unique niches of expertise, coming together
+							to deliver exceptional results through shared passion, curiosity and
+							<span className={styles.hightlight}> meticulous</span> planning, we work closely with our
+							clients during each stage of the project to comprehend their distinctive requirements and
+							provide excellent user experiences for their users. Through our expertise and inventive
+							solutions, we carve a path that leads to a product that exceeds expectations. Our proven
+							track record and unwavering commitment to improving our designs and ingenuity is a testament
+							to our dedication to deliver excellence in every project we choose to undertake. When you
+							work with us, you can be confident that you are partnering with a team of true professionals
+							who will help you achieve your goals and take your brand to the next level.
+						</p>
+					</div>
+				</div>
 
-        <div className={styles.section} id={styles.ourStory}>
-          <div className={styles.headerText}>
-            <h3 style={{ color: "#FFA500" }}>Our Story</h3>
-          </div>
+				<div className={styles.section} id={styles.ourStory}>
+					<div className={styles.headerText}>
+						<h3 style={{ color: '#FFA500' }}>Our Story</h3>
+					</div>
 
-          <div className={styles.imgContainer}>
-            <BlurHashImages
-              src={story}
-              style={imageStyles}
-              hashCode={storyHash}
-            />
-            <p>
-              As a team of experienced professional software engineers, we bring
-              our passion for problem-solving and ingenuity to each project we
-              decide to undertake. With a keen attention to detail, and
-              insatiable curiosity, we work closely with our select clients
-              throughout the entire development process to gain a deep
-              understanding of their unique needs. Our goal is to deliver a
-              seamless user experience, creating unique and memorable brands for
-              our clients that not only meets but exceeds users and clients
-              expectations. We take great pride in our ability to tackle complex
-              challenges and provide customized solutions that drive our
-              clients' success.
-            </p>
-          </div>
-        </div>
-      </div>
-      <Team2 />
-      <Footer />
-    </motion.div>
-  );
+					<div className={styles.imgContainer}>
+						<BlurHashImages src={story} style={imageStyles} hashCode={storyHash} />
+						<p>
+							As a team of experienced professional software engineers, we bring our passion for
+							problem-solving and ingenuity to each project we decide to undertake. With a keen attention
+							to detail, and insatiable curiosity, we work closely with our select clients throughout the
+							entire development process to gain a deep understanding of their unique needs. Our goal is
+							to deliver a seamless user experience, creating unique and memorable brands for our clients
+							that not only meets but exceeds users and clients expectations. We take great pride in our
+							ability to tackle complex challenges and provide customized solutions that drive our
+							clients' success.
+						</p>
+					</div>
+				</div>
+			</div>
+			<Team2 />
+			<Footer />
+		</motion.div>
+	);
 };
 
 export default About;
--- client/src/Pages/Landing Page/components/Offer/Offer.tsx
+++ client/src/Pages/Landing Page/components/Offer/Offer.tsx
@@ -1,180 +1,152 @@
-import React, { useContext } from "react";
-import styles from "./Offer.module.scss";
-import { motion } from "framer-motion";
-import { BsArrowRightCircle } from "react-icons/bs";
-import codeDesign from "../../../../assets/Home/codeDesign.webp";
-import uxDesign from "../../../../assets/Home/uxDesign.webp";
-import webDesign from "../../../../assets/Home/webDesign.webp";
-import { HashContext } from "../../../../components/BlurHashEncoder/BlurHashDecoder";
-import { Link } from "react-router-dom";
-import BlurHashImages from "./components/BlurHashImages";
+import React, { useContext } from 'react';
+import styles from './Offer.module.scss';
+import { motion } from 'framer-motion';
+import { BsArrowRightCircle } from 'react-icons/bs';
+import codeDesign from '../../../../assets/Home/codeDesign.webp';
+import uxDesign from '../../../../assets/Home/uxDesign.webp';
+import webDesign from '../../../../assets/Home/webDesign.webp';
+import { HashContext } from '../../../../components/BlurHashEncoder/BlurHashDecoder';
+import { Link } from 'react-router-dom';
+import BlurHashImages from './components/BlurHashImages';
 
 interface ImageType {
-  [name: string]: {
-    url: string;
-    hash: string;
-  };
+	[name: string]: {
+		url: string;
+		hash: string;
+	};
 }
 
 const Offer = () => {
-  const hashData = useContext<ImageType>(HashContext);
-  console.log("Hash Data: ", hashData);
-  const softwareHash = hashData["laptop"].hash;
-  const uxHash = hashData["tablet"].hash;
-  const webHash = hashData["imac"].hash;
+	const hashData = useContext<ImageType>(HashContext);
+	console.log('Hash Data: ', hashData);
+	const softwareHash = hashData['laptop'].hash;
+	const uxHash = hashData['tablet'].hash;
+	const webHash = hashData['imac'].hash;
 
-  return (
-    <div id="services" className={styles.mainContainer}>
-      <div className={styles.container}>
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1, transition: { duration: 1 } }}
-          viewport={{ once: true }}
-          className={styles.cta}
-        >
-          <h3>Software Services</h3>
-          <h3 style={{ color: "#efff" }}>Custom Solutions to Suit You</h3>
-          <hr className={styles.break} />
-          <br />
-          <br />
-          <p className={styles.paragraph}>
-            Unlock Your Business Potential: Our versatile team of Software
-            Developers, UX Designers, and Project Managers collaborate
-            seamlessly to transform your vision into reality and propel your
-            business objectives forward.
-          </p>
+	return (
+		<div id="services" className={styles.mainContainer}>
+			<div className={styles.container}>
+				<motion.div
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1, transition: { duration: 1 } }}
+					viewport={{ once: true }}
+					className={styles.cta}
+				>
+					<h3>Software Services</h3>
+					<h3 style={{ color: '#efff' }}>Custom Solutions to Suit You</h3>
+					<hr className={styles.break} />
+					<br />
+					<br />
+					<p className={styles.paragraph}>
+						Finding the right service to meet your business needs can be challenging. That’s why we work
+						closely with you to understand your specific requirements and offer you a service that will make
+						an impact on your product.
+					</p>
 
-          <Link
-            to={"/contact"}
-            className={styles.link}
-            title="Takes customer to the contact form allowing communication with Pixel Koi Company"
-          >
-            <BsArrowRightCircle
-              style={{ color: "#e8985c", alignSelf: "center" }}
-              size={30}
-            />
-            <p style={{ margin: "auto 0", color: "#fff" }}>Let's Talk</p>
-          </Link>
-        </motion.div>
+					<Link
+						to={'/contact'}
+						className={styles.link}
+						title="Takes customer to the contact form allowing communication with Pixel Koi Company"
+					>
+						<BsArrowRightCircle style={{ color: '#e8985c', alignSelf: 'center' }} size={30} />
+						<p style={{ margin: 'auto 0', color: '#fff' }}>Let's Talk</p>
+					</Link>
+				</motion.div>
 
-        <div className={styles.softwareGroup}>
-          <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { duration: 1 } }}
-            viewport={{ once: true }}
-            className={styles.catDescription}
-          >
-            <h3 style={{ color: "#efff" }}>Software Engineering</h3>
-            <hr className={styles.break} />
-            <br />
-            <br />
-            <p>
-              Our custom software development solutions meet your specific needs
-              and goals, giving you a competitive advantage. Our comprehensive
-              end-to-end software development services cover everything from
-              planning, designing and developing to testing to launching,
-              relieving you from the burden of managing intricate SDLCs.
-            </p>
-            <Link
-              to={"/contact"}
-              className={styles.link}
-              title="Takes customer to the contact form allowing communication with Pixel Koi Company"
-            >
-              <BsArrowRightCircle
-                style={{ color: "#e8985c", alignSelf: "center" }}
-                size={30}
-              />
-              <p style={{ margin: "auto 0", color: "#fff" }}>Learn More</p>
-            </Link>
-          </motion.div>
-          <div className={styles.cardIMG}>
-            <BlurHashImages
-              src={codeDesign}
-              style={{ marginLeft: "auto" }}
-              hashCode={softwareHash}
-            />
-          </div>
-        </div>
+				<div className={styles.softwareGroup}>
+					<motion.div
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1, transition: { duration: 1 } }}
+						viewport={{ once: true }}
+						className={styles.catDescription}
+					>
+						<h3 style={{ color: '#efff' }}>Software Engineering</h3>
+						<hr className={styles.break} />
+						<br />
+						<br />
+						<p>
+							Our custom software development solutions meet your specific needs and goals, giving you a
+							competitive advantage. Our comprehensive end-to-end software development services cover
+							everything from planning, designing and developing to testing to launching, relieving you
+							from the burden of managing intricate SDLCs.
+						</p>
+						<Link
+							to={'/contact'}
+							className={styles.link}
+							title="Takes customer to the contact form allowing communication with Pixel Koi Company"
+						>
+							<BsArrowRightCircle style={{ color: '#e8985c', alignSelf: 'center' }} size={30} />
+							<p style={{ margin: 'auto 0', color: '#fff' }}>Learn More</p>
+						</Link>
+					</motion.div>
+					<div className={styles.cardIMG}>
+						<BlurHashImages src={codeDesign} style={{ marginLeft: 'auto' }} hashCode={softwareHash} />
+					</div>
+				</div>
 
-        <div className={styles.uxGroup}>
-          <div className={styles.cardIMG}>
-            <BlurHashImages
-              src={uxDesign}
-              hashCode={uxHash}
-              style={{ marginRight: "auto" }}
-            />
-          </div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { duration: 1 } }}
-            viewport={{ once: true }}
-            className={styles.catDescription}
-          >
-            <h3 style={{ color: "#efff" }}>UX Design</h3>
-            <hr className={styles.break} />
-            <br />
-            <br />
-            <p>
-              If you seek a team that values transparent design processes and
-              consistently delivers exceptional results within your budget? Turn
-              to our long-standing design team specializing in responsive web
-              and mobile solutions, and UX strategy and consulting.
-            </p>
-            <Link
-              to={"/contact"}
-              className={styles.link}
-              title="Takes customer to the contact form allowing communication with Pixel Koi Company"
-            >
-              <BsArrowRightCircle
-                style={{ color: "#e8985c", alignSelf: "center" }}
-                size={30}
-              />
-              <p style={{ margin: "auto 0", color: "#fff" }}>Learn More</p>
-            </Link>
-          </motion.div>
-        </div>
+				<div className={styles.uxGroup}>
+					<div className={styles.cardIMG}>
+						<BlurHashImages src={uxDesign} hashCode={uxHash} style={{ marginRight: 'auto' }} />
+					</div>
+					<motion.div
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1, transition: { duration: 1 } }}
+						viewport={{ once: true }}
+						className={styles.catDescription}
+					>
+						<h3 style={{ color: '#efff' }}>UX Design</h3>
+						<hr className={styles.break} />
+						<br />
+						<br />
+						<p>
+							If you seek a team that values transparent design processes and consistently delivers
+							exceptional results within your budget? Turn to our long-standing design team specializing
+							in responsive web and mobile solutions, and UX strategy and consulting.
+						</p>
+						<Link
+							to={'/contact'}
+							className={styles.link}
+							title="Takes customer to the contact form allowing communication with Pixel Koi Company"
+						>
+							<BsArrowRightCircle style={{ color: '#e8985c', alignSelf: 'center' }} size={30} />
+							<p style={{ margin: 'auto 0', color: '#fff' }}>Learn More</p>
+						</Link>
+					</motion.div>
+				</div>
 
-        <div className={styles.softwareGroup}>
-          <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { duration: 1 } }}
-            viewport={{ once: true }}
-            className={styles.catDescription}
-          >
-            <h3 style={{ color: "#efff" }}>Website Development</h3>
-            <hr className={styles.break} />
-            <br />
-            <br />
-            <p>
-              Pixel Koi is dedicated to offering all-encompassing website
-              development and ecommerce solutions to businesses of all sizes.
-              Our professional team of developers, designers, and project
-              managers collaborate closely to design custom solutions that cater
-              to the unique needs of each client.
-            </p>
-            <Link
-              to={"/contact"}
-              className={styles.link}
-              title="Takes customer to the contact form allowing communication with Pixel Koi Company"
-            >
-              <BsArrowRightCircle
-                style={{ color: "#e8985c", alignSelf: "center" }}
-                size={30}
-              />
-              <p style={{ margin: "auto 0", color: "#fff" }}>Learn More</p>
-            </Link>
-          </motion.div>
-          <div className={styles.cardIMG} style={{}}>
-            <BlurHashImages
-              src={webDesign}
-              style={{ marginLeft: "auto" }}
-              hashCode={webHash}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				<div className={styles.softwareGroup}>
+					<motion.div
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1, transition: { duration: 1 } }}
+						viewport={{ once: true }}
+						className={styles.catDescription}
+					>
+						<h3 style={{ color: '#efff' }}>Website Development</h3>
+						<hr className={styles.break} />
+						<br />
+						<br />
+						<p>
+							Pixel Koi is dedicated to offering all-encompassing website development and ecommerce
+							solutions to businesses of all sizes. Our professional team of developers, designers, and
+							project managers collaborate closely to design custom solutions that cater to the unique
+							needs of each client.
+						</p>
+						<Link
+							to={'/contact'}
+							className={styles.link}
+							title="Takes customer to the contact form allowing communication with Pixel Koi Company"
+						>
+							<BsArrowRightCircle style={{ color: '#e8985c', alignSelf: 'center' }} size={30} />
+							<p style={{ margin: 'auto 0', color: '#fff' }}>Learn More</p>
+						</Link>
+					</motion.div>
+					<div className={styles.cardIMG} style={{}}>
+						<BlurHashImages src={webDesign} style={{ marginLeft: 'auto' }} hashCode={webHash} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Offer;
