cc6e899375e1cffddbe2dcdea0f5da3756dd38ad
Garabed96 <garonazarian09@gmail.com>
2023-04-20 00:57:56 +0700

    vite update (moving files around)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: "build",
+    },
+    plugins: [react()],
+  };
 });
