acc277e93834d695bb17d8072a682ad574361255
Garabed96 <garonazarian09@gmail.com>
2023-04-21 20:06:37 +0700

    smolfix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,38 +5,11 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    base: "./",
     build: {
       outDir: "dist",
       assetsInclude: ["registerSW.js"],
     },
-    plugins: [
-      react(),
-      VitePWA({
-        manifest: {
-          name: "Pixel Koi Company",
-          short_name: "Pixel Koi",
-          description:
-            "PixelKoi.com: Web Development, Mobile Development, UX/UI Design",
-          icons: [
-            {
-              src: "dist/pixelKoi.png",
-              type: "image/png",
-              sizes: "192x192",
-              purpose: "any maskable",
-            },
-            {
-              src: "dist/pixelKoiLarge.png",
-              type: "image/png",
-              sizes: "520x520",
-              purpose: "any maskable",
-            },
-          ],
-          start_url: "/",
-          display: "standalone",
-          background_color: "#ffffff",
-          theme_color: "#2196f3",
-        },
-      }),
-    ],
+    plugins: [react(), VitePWA({})],
   };
 });
