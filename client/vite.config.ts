64d69ad3da9482d11359497319eeb5266314300b
Garabed96 <garonazarian09@gmail.com>
2023-04-22 22:39:10 +0700

    PWA Working on Production

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
@@ -5,48 +6,45 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    base: "./",
-    build: {
-      rollupOptions: {
-        input: {
-          main: "./index.html",
-        },
-        output: {
-          dir: "dist",
-        },
-      },
-      outDir: "dist",
-      assetsInclude: ["registerSW.js"],
+    includeAssets: ["**/*.png", "public/*.png"],
+    workbox: {
+      globPatterns: ["**/*.{js,css,html,ico,jpg,,png,svg,webmanifest}"],
     },
     plugins: [
       react(),
       VitePWA({
+        registerType: "autoUpdate",
         manifest: {
           name: "Pixel Koi Company",
           short_name: "Pixel Koi",
+          display: "standalone",
+          background_color: "#ffffff",
+          theme_color: "#2196f3",
           description:
             "PixelKoi.com: Web Development, Mobile Development, UX/UI Design",
           icons: [
             {
-              src: "/assets/pixelKoi.png",
+              src: "/img/icons/pixelKoi.png",
               type: "image/png",
               sizes: "192x192",
               purpose: "any maskable",
             },
             {
-              src: "/assets/pixelKoiLarge.png",
+              src: "img/icons/pixelKoiLarge.png",
               type: "image/png",
               sizes: "520x520",
               purpose: "any maskable",
             },
           ],
-          start_url: "/",
-          display: "standalone",
-          background_color: "#ffffff",
-          theme_color: "#2196f3",
         },
-        outDir: "dist",
       }),
     ],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+        },
+      },
+    },
   };
 });
