bba13d6f3578702858a3d08f6b3b277ed0d70036
Jonatahn Bajada <jbajada@ryerson.ca>
2023-04-30 17:23:23 -0400

    updated blog page

--- client/src/Pages/Blog/Blog.tsx
+++ client/src/Pages/Blog/Blog.tsx
@@ -1,50 +1,53 @@
 import React from 'react';
 import styles from './Blog.module.scss';
 import Nav from '../../components/Nav/Nav';
+import Footer from '../../components/Footer/Main/Footer';
 import Card from './component/Card';
 import HeaderCard from './component/HeaderCard';
-import img from '../../assets/Home/web.jpg';
 
 const tags = [ 'UX Design', 'AI', 'Art' ];
 const Blog = () => {
 	return (
-		<div className={styles.wrapper}>
-			<Nav />
-			<HeaderCard />
-			<div className={styles.cardWrapper}>
-				<div className={styles.cardContainer}>
-					<div className={styles.gridItem}>
-						<Card
-							title={'AI will kill all'}
-							date={'May 16th, 1991'}
-							description={
-								'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
-							}
-							tags={tags}
-						/>
-					</div>
-					<div className={styles.gridItem}>
-						<Card
-							title={'AI will kill all'}
-							date={'May 16th, 1991'}
-							description={
-								'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
-							}
-							tags={tags}
-						/>
-					</div>
-					<div className={styles.gridItem}>
-						<Card
-							title={'AI will kill all'}
-							date={'May 16th, 1991'}
-							description={
-								'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
-							}
-							tags={tags}
-						/>
+		<div className={styles.mainWrapper}>
+			{/* <Nav /> */}
+			<div className={styles.wrapper}>
+				<HeaderCard />
+				<div className={styles.cardWrapper}>
+					<div className={styles.cardContainer}>
+						<div className={styles.gridItem}>
+							<Card
+								title={'AI will kill all'}
+								date={'May 16th, 1991'}
+								description={
+									'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
+								}
+								tags={tags}
+							/>
+						</div>
+						<div className={styles.gridItem}>
+							<Card
+								title={'AI will kill all'}
+								date={'May 16th, 1991'}
+								description={
+									'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
+								}
+								tags={tags}
+							/>
+						</div>
+						<div className={styles.gridItem}>
+							<Card
+								title={'AI will kill all'}
+								date={'May 16th, 1991'}
+								description={
+									'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
+								}
+								tags={tags}
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
+			{/* <Footer /> */}
 		</div>
 	);
 };
--- client/src/Pages/Blog/component/Card.tsx
+++ client/src/Pages/Blog/component/Card.tsx
@@ -6,19 +6,23 @@ const Card = (props) => {
 	const tags = props.tags;
 
 	return (
-		<div className={styles.container}>
-			<img src={img} alt="" />
-			<div className={styles.infoContainer}>
-				<p className={styles.author}>Jonathan Bajada &#x2022; {props.date}</p>
-				<h4>{props.title}</h4>
-				<p className={styles.description}>{props.description}</p>
-			</div>
-			<div className={styles.tagWrapper}>
-				{tags.map((tag, index) => (
-					<div className={styles.tagContainer} key={index}>
-						<span className={styles.tagName}>{tag}</span>
-					</div>
-				))}
+		<div className={styles.container} onClick={{}}>
+			<div style={{ width: '100%' }}>
+				<img src={img} alt="" />
+				<div className={styles.infoContainer}>
+					<b>
+						<p className={styles.author}>Jonathan Bajada &#x2022; {props.date}</p>
+					</b>
+					<h4>{props.title}</h4>
+					<p className={styles.description}>{props.description}</p>
+				</div>
+				<div className={styles.tagWrapper}>
+					{tags.map((tag, index) => (
+						<div className={styles.tagContainer} key={index}>
+							<span className={styles.tagName}>{tag}</span>
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	);
