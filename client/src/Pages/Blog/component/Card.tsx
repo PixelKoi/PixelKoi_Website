74babf6f356874ec9ec6a64785568dc454c0dc58
Jonatahn Bajada <jbajada@ryerson.ca>
2023-04-30 15:34:24 -0400

    added atgs

--- client/src/Pages/Blog/component/Card.tsx
+++ client/src/Pages/Blog/component/Card.tsx
@@ -3,13 +3,22 @@ import styles from './Card.module.scss';
 import img from '../../../assets/Home/web.jpg';
 
 const Card = (props) => {
+	const tags = [ 'UX Design', 'AI', 'Art' ];
+
 	return (
 		<div className={styles.container}>
 			<img src={img} alt="" />
 			<div className={styles.infoContainer}>
-				<p>Jonathan Bajada &#x2022; {props.date}</p>
+				<p className={styles.author}>Jonathan Bajada &#x2022; {props.date}</p>
 				<h4>{props.title}</h4>
-				<p>{props.description}</p>
+				<p className={styles.description}>{props.description}</p>
+			</div>
+			<div className={styles.tagWrapper}>
+				{tags.map((tag, index) => (
+					<div className={styles.tagContainer} key={index}>
+						<span className={styles.tagName}>{tag}</span>
+					</div>
+				))}
 			</div>
 		</div>
 	);
