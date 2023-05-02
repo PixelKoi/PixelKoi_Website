39e23ed8848549d34f731ee6475087fde2bdecc7
Garabed96 <garonazarian09@gmail.com>
2023-05-02 21:54:07 +0700

    supabase initialization

--- client/src/Pages/Blog/Blog.tsx
+++ client/src/Pages/Blog/Blog.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import styles from './Blog.module.scss';
-import Nav from '../../components/Nav/Nav';
-import Footer from '../../components/Footer/Main/Footer';
-import Card from './component/Card';
-import HeaderCard from './component/HeaderCard';
-import { FaLongArrowAltLeft, FaLongArrowAltRight } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import img from '../../assets/Home/web.jpg';
-import img2 from '../../assets/Home/box.jpg';
+import React from "react";
+import styles from "./Blog.module.scss";
+import Nav from "../../components/Nav/Nav";
+import Footer from "../../components/Footer/Main/Footer";
+import Card from "./component/Card";
+import HeaderCard from "./component/HeaderCard";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import img from "../../assets/Home/web.jpg";
+import img2 from "../../assets/Home/box.jpg";
+import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL, SUPABASE_API_KEY } from "../../../config";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+
 //Todo
 /*
    Onclick update global variables to be shown on blog page using redux
@@ -15,79 +20,93 @@ import img2 from '../../assets/Home/box.jpg';
    Add pagenation
 */
 
-const tags = [ 'UX Design', 'AI', 'Art' ];
+const tags = ["UX Design", "AI", "Art"];
 const Blog = () => {
-	return (
-		<div className={styles.mainWrapper}>
-			<Nav />
-			<div className={styles.wrapper}>
-				<h1>Musings from our collective</h1>
-				<br />
-				<h6>The latest industry news, interviews, technologies, and resources</h6>
-				<HeaderCard
-					title={'AI will kill all'}
-					date={'May 16th, 1991'}
-					description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'}
-					tags={tags}
-					img={img2}
-				/>
-				<div className={styles.cardWrapper}>
-					<div className={styles.cardContainer}>
-						<Link to="/bpage" className={styles.gridItem}>
-							<Card
-								title={'AI will kill all'}
-								date={'May 16th, 1991'}
-								description={
-									'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
-								}
-								tags={tags}
-								img={img}
-							/>
-						</Link>
-						<Link to="/bpage" className={styles.gridItem}>
-							<Card
-								title={'AI will kill all'}
-								date={'May 16th, 1991'}
-								description={
-									'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
-								}
-								tags={tags}
-								img={img}
-							/>
-						</Link>
-						<Link to="/bpage" className={styles.gridItem}>
-							<Card
-								title={'AI will kill all'}
-								date={'May 16th, 1991'}
-								description={
-									'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
-								}
-								tags={tags}
-								img={img}
-							/>
-						</Link>
-					</div>
-				</div>
-				{/* Add Pagenation */}
-				<div className={styles.pageContainer}>
-					<div className={styles.leftArrow}>
-						<div className={styles.icon}>
-							<FaLongArrowAltLeft size={35} />
-						</div>
-						<p>Previous</p>
-					</div>
-					<div className={styles.rightArrow}>
-						<p>Next</p>
-						<div className={styles.icon}>
-							<FaLongArrowAltRight size={35} />
-						</div>
-					</div>
-				</div>
-			</div>
+  // function
+  async function blogData() {
+    const { data, error } = await supabase.from("Blog").select("*");
+    try {
+      console.log("what the fuck is this", data);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
-			<Footer />
-		</div>
-	);
+  return (
+    <div className={styles.mainWrapper}>
+      <Nav />
+      <div className={styles.wrapper}>
+        <h1>Musings from our collective</h1>
+        <br />
+        <h6>
+          The latest industry news, interviews, technologies, and resources
+        </h6>
+        <HeaderCard
+          title={"AI will kill all"}
+          date={"May 16th, 1991"}
+          description={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+          }
+          tags={tags}
+          img={img2}
+        />
+        <div className={styles.cardWrapper}>
+          <div className={styles.cardContainer}>
+            <Link to="/bpage" className={styles.gridItem}>
+              <Card
+                title={"AI will kill all"}
+                date={"May 16th, 1991"}
+                description={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+                }
+                tags={tags}
+                img={img}
+              />
+            </Link>
+            <Link to="/bpage" className={styles.gridItem}>
+              <Card
+                title={"AI will kill all"}
+                date={"May 16th, 1991"}
+                description={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+                }
+                tags={tags}
+                img={img}
+              />
+            </Link>
+            <Link to="/bpage" className={styles.gridItem}>
+              <Card
+                title={"AI will kill all"}
+                date={"May 16th, 1991"}
+                description={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+                }
+                tags={tags}
+                img={img}
+              />
+            </Link>
+          </div>
+        </div>
+        {/* Add Pagenation */}
+        <div className={styles.pageContainer}>
+          <div className={styles.leftArrow}>
+            <div className={styles.icon}>
+              <FaLongArrowAltLeft size={35} />
+            </div>
+            <p>Previous</p>
+          </div>
+          <div className={styles.rightArrow}>
+            <p>Next</p>
+            <div className={styles.icon}>
+              <FaLongArrowAltRight size={35} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Blog;
