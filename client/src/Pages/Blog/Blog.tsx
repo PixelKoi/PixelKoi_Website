95465df61499e0d85d180d9aa6d4ae2ae7d660b6
Garabed96 <garonazarian09@gmail.com>
2023-05-03 21:40:36 +0700

    Passing Images to cards, (error handing added)

--- client/src/Pages/Blog/Blog.tsx
+++ client/src/Pages/Blog/Blog.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import styles from './Blog.module.scss';
-import Nav from '../../components/Nav/Nav';
-import Footer from '../../components/Footer/Main/Footer';
-import Card from './component/Card';
-import HeaderCard from './component/HeaderCard';
-import { FaLongArrowAltLeft, FaLongArrowAltRight } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import img from '../../assets/Home/web.jpg';
-import img2 from '../../assets/Home/box.jpg';
-import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_API_KEY } from '../../../config';
+import React, { useEffect, useState } from "react";
+import styles from "./Blog.module.scss";
+import Nav from "../../components/Nav/Nav";
+import Footer from "../../components/Footer/Main/Footer";
+import Card from "./component/Card";
+import HeaderCard from "./component/HeaderCard";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import img from "../../assets/Home/web.jpg";
+import img2 from "../../assets/Home/box.jpg";
+import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL, SUPABASE_API_KEY } from "../../../config";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
 
@@ -20,18 +20,18 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
    Add pagenation
 */
 interface BlogData {
-	title: string;
-	content: string;
-	author: string;
-	image_id: Number;
-	Images: Any;
+  title: string;
+  content: string;
+  author: string;
+  image_id: Number;
+  Images: Any;
 }
-const tags = [ 'UX Design', 'AI', 'Art' ];
+const tags = ["UX Design", "AI", "Art"];
 const Blog = () => {
-	const [ blogData, setBlogData ] = useState<BlogData[] | null>(null);
-	const [ blogError, setBlogError ] = useState<string | null>(null);
-	{
-		/*
+  const [blogData, setBlogData] = useState<BlogData[] | null>(null);
+  const [blogError, setBlogError] = useState<string | null>(null);
+  {
+    /*
         Blog Columns:  author, title, content, date, blog_post_id
         Images Foreign Key: blog_post_id, image_url, image_id
         Each Blog post can have many Images: Blog to Images, One-To-Many Relationship
@@ -39,115 +39,129 @@ const Blog = () => {
         TODO: react-html-parser to dynamically insert images in random locations within your blog post content
         TODO: Blog Posts mapped to each card dynamically
       */
-	}
-	useEffect(() => {
-		const fetchBlogData = async () => {
-			const { data, error } = await supabase.from('Blog').select(`*, Images(image_url, image_id)`);
-			if (error) {
-				setBlogData(null);
-				setBlogError("Couldn't get Blog Data, sorry try again later.");
-				console.log(error);
-			}
-			if (data) {
-				console.log('THIIS IS DATA:', data);
-				// @ts-ignore
-				setBlogData(data);
-				setBlogError(null);
-			}
-		};
-		fetchBlogData();
-	}, []);
+  }
+  useEffect(() => {
+    const fetchBlogData = async () => {
+      const { data, error } = await supabase
+        .from("Blog")
+        .select(`*, Images(image_url, image_id)`);
+      if (error) {
+        setBlogData(null);
+        setBlogError("Couldn't get Blog Data, sorry try again later.");
+        console.log(error);
+      }
+      if (data) {
+        console.log("THIIS IS DATA:", data);
+        // @ts-ignore
+        setBlogData(data);
+        setBlogError(null);
+      }
+    };
+    fetchBlogData();
+  }, []);
 
-	//need to save blog_post_id when clicking link so that we can grab the correct blog from the supabase db
-	const loadBlogColumns = () => {
-		if (blogData === null) {
-			return null;
-		}
-		return blogData.map((blog) => {
-			return (
-				<Link to="/bpage" className={styles.gridItem} key={blog.blog_post_id}>
-					<Card
-						author={blog.author}
-						date={blog.date}
-						title={blog.title}
-						description={blog.content}
-						tags={tags}
-						img={img}
-					/>
-				</Link>
-			);
-		});
-	};
+  //need to save blog_post_id when clicking link so that we can grab the correct blog from the supabase db
+  const loadBlogColumns = () => {
+    if (blogData === null) {
+      return null;
+    }
+    return blogData.map((blog) => {
+      let imageUrl = "";
+      for (let i = 0; i < blog.Images.length; i++) {
+        if (blog.Images[i].image_url) {
+          imageUrl = blog.Images[i].image_url;
+          break;
+        }
+      }
+      return (
+        <Link to="/bpage" className={styles.gridItem} key={blog.blog_post_id}>
+          <Card
+            author={blog.author}
+            date={blog.date}
+            title={blog.title}
+            description={blog.content}
+            tags={tags}
+            img={imageUrl}
+            imgStyle={{ maxWidth: "500px", maxHeight: "500px" }}
+          />
+        </Link>
+      );
+    });
+  };
 
-	return (
-		<div className={styles.mainWrapper}>
-			<Nav />
-			<div className={styles.wrapper}>
-				{blogError && <p>{blogError}</p>}
-				{blogData && (
-					<ul>
-						{blogData.map((blog) => (
-							<div key={blog.blog_post_id}>
-								<li>
-									{blog.title} by {blog.author}
-								</li>
-								<ul>
-									{blog.Images.map(
-										(image) =>
-											image.image_url && (
-												<li key={image.image_id}>
-													{image.image_url}
-													<img
-														src={image.image_url}
-														alt="Blog post"
-														style={{
-															maxWidth: '250px',
-															maxHeight: '500px',
-															padding: '1rem'
-														}}
-													/>
-												</li>
-											)
-									)}
-								</ul>
-							</div>
-						))}
-					</ul>
-				)}
+  return (
+    <div className={styles.mainWrapper}>
+      <Nav />
+      <div className={styles.wrapper}>
+        {blogError && <p>{blogError}</p>}
+        {blogData && (
+          <ul>
+            {blogData.map((blog) => (
+              <div key={blog.blog_post_id}>
+                <li>
+                  {blog.title} by {blog.author}
+                </li>
+                <ul>
+                  {blog.Images.map(
+                    (image) =>
+                      image.image_url && (
+                        <li key={image.image_id}>
+                          {image.image_url}
+                          <img
+                            src={image.image_url}
+                            alt="Blog post"
+                            style={{
+                              maxWidth: "250px",
+                              maxHeight: "500px",
+                              padding: "1rem",
+                            }}
+                          />
+                        </li>
+                      )
+                  )}
+                </ul>
+              </div>
+            ))}
+          </ul>
+        )}
 
-				<h1>Musings from our collective</h1>
-				<br />
-				<h6>The latest industry news, interviews, technologies, and resources</h6>
-				<HeaderCard
-					title={'AI will kill all'}
-					date={'May 16th, 1991'}
-					description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'}
-					tags={tags}
-					img={img2}
-				/>
-				<div className={styles.cardWrapper}>
-					<div className={styles.cardContainer}>{loadBlogColumns()}</div>
-				</div>
-				{/* Add Pagenation */}
-				<div className={styles.pageContainer}>
-					<div className={styles.leftArrow}>
-						<div className={styles.icon}>
-							<FaLongArrowAltLeft size={35} />
-						</div>
-						<p>Previous</p>
-					</div>
-					<div className={styles.rightArrow}>
-						<p>Next</p>
-						<div className={styles.icon}>
-							<FaLongArrowAltRight size={35} />
-						</div>
-					</div>
-				</div>
-			</div>
+        <h1>Musings from our collective</h1>
+        <br />
+        <h6>
+          The latest industry news, interviews, technologies, and resources
+        </h6>
+        <HeaderCard
+          title={"AI will kill all"}
+          date={"May 16th, 1991"}
+          description={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+          }
+          tags={tags}
+          img={img2}
+        />
+        <div className={styles.cardWrapper}>
+          <div className={styles.cardContainer}>{loadBlogColumns()}</div>
+        </div>
+        {/* Add Pagenation */}
+        <div className={styles.pageContainer}>
+          <div className={styles.leftArrow}>
+            <div className={styles.icon}>
+              <FaLongArrowAltLeft size={35} />
+            </div>
+            <p>Previous</p>
+          </div>
+          <div className={styles.rightArrow}>
+            <p>Next</p>
+            <div className={styles.icon}>
+              <FaLongArrowAltRight size={35} />
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<Footer />
-		</div>
-	);
+      <Footer />
+    </div>
+  );
 };
 
 export default Blog;
