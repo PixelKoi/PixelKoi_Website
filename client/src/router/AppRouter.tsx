900f907eee85189e814d54aa38bb3f61d2d3951a
Garabed96 <garonazarian09@gmail.com>
2023-05-04 12:16:11 +0700

    routing blogs to blog pages

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -1,22 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
-import Landing from '../Pages/Landing Page/Landing';
-import About from '../Pages/About/About';
-import Blog from '../Pages/Blog/Blog';
-import BlogPage from '../Pages/Blog/BlogPage';
-import { Contact } from '../Pages/Contact/Contact2';
-import BlurHashDecoder from '../components/BlurHashEncoder/BlurHashDecoder';
+import { Route, Routes } from "react-router-dom";
+import Landing from "../Pages/Landing Page/Landing";
+import About from "../Pages/About/About";
+import Blog from "../Pages/Blog/Blog";
+import BlogPage from "../Pages/Blog/BlogPage";
+import { Contact } from "../Pages/Contact/Contact2";
+import BlurHashDecoder from "../components/BlurHashEncoder/BlurHashDecoder";
 const AppRouter = () => {
-	return (
-		<BlurHashDecoder>
-			<Routes>
-				<Route path="/" element={<Landing />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="/blog" element={<Blog />} />
-				<Route path="/bpage" element={<BlogPage />} />
-			</Routes>
-		</BlurHashDecoder>
-	);
+  return (
+    <BlurHashDecoder>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<BlogPage />} />
+      </Routes>
+    </BlurHashDecoder>
+  );
 };
 
 export default AppRouter;
