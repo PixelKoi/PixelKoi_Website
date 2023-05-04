a9f90b9edb089af6aaa76a84c2f813065b04db16
Garabed96 <garonazarian09@gmail.com>
2023-05-04 18:47:36 +0700

    back button

--- client/src/Pages/Blog/BlogPost.tsx
+++ client/src/Pages/Blog/BlogPost.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./BlogPage.module.scss";
 import Nav from "../../components/Nav/Nav";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const BlogPost = () => {
   const styles = {
@@ -52,11 +52,25 @@ const BlogPost = () => {
       width: "750px",
       maxHeight: "500px",
     },
+    button: {
+      padding: "0.75rem 1.25rem",
+      fontSize: "1rem",
+      lineHeight: "1.5",
+      borderRadius: "0.25rem",
+      backgroundColor: "#007bff",
+      color: "#fff",
+      border: "none",
+      boxShadow: "0 0.5rem 1rem rgba(0, 0, 0, 0.15)",
+      cursor: "pointer",
+      transition: "all 0.15s ease-in-out",
+      marginTop: "1rem",
+    },
   };
 
   // @ts-ignore
   const { state } = useLocation<{ data: BlogData }>();
   const { data } = state;
+  const navigate = useNavigate();
   let imageUrl = "";
   for (let i = 0; i < data.Images.length; i++) {
     if (data.Images[i].image_url) {
@@ -67,6 +81,9 @@ const BlogPost = () => {
   console.log("imageUrl: ", imageUrl);
   return (
     <div style={styles.container}>
+      <button style={styles.button} onClick={() => navigate(-1)}>
+        Back to All Blogs
+      </button>
       {imageUrl && <img src={imageUrl} alt="blog post" style={styles.image} />}
       <h1 style={styles.title}>{data.title}</h1>
       <h3 style={styles.author}>By {data.author}</h3>
