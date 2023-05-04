029d71b59a9063d20517bf7ebd781b5748a24bef
Garabed96 <garonazarian09@gmail.com>
2023-05-04 17:54:25 +0700

    simple styling blog posts

--- client/src/Pages/Blog/BlogPost.tsx
+++ client/src/Pages/Blog/BlogPost.tsx
@@ -4,15 +4,51 @@ import Nav from "../../components/Nav/Nav";
 import { useLocation } from "react-router-dom";
 
 const BlogPost = () => {
+  const styles = {
+    container: {
+      maxWidth: "800px",
+      margin: "10rem auto",
+      padding: "20px",
+      backgroundColor: "#f9f9f9",
+      boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.1)",
+      borderRadius: "5px",
+      fontFamily: "Arial, sans-serif",
+    },
+    title: {
+      fontSize: "32px",
+      fontWeight: "bold",
+      marginBottom: "10px",
+      color: "#333",
+    },
+    author: {
+      fontSize: "18px",
+      fontWeight: "bold",
+      marginBottom: "5px",
+      color: "#666",
+    },
+    date: {
+      fontSize: "16px",
+      fontWeight: "normal",
+      marginBottom: "20px",
+      color: "#999",
+    },
+    content: {
+      fontSize: "18px",
+      lineHeight: "28px",
+      color: "#333",
+    },
+  };
+
   // @ts-ignore
   const { state } = useLocation<{ data: BlogData }>();
   const { data } = state;
 
   return (
-    <div>
-      <h1>{data.title}</h1>
-      <p>{data.content}</p>
-      {/* ... */}
+    <div style={styles.container}>
+      <h1 style={styles.title}>{data.title}</h1>
+      <h3 style={styles.author}>By {data.author}</h3>
+      <h3 style={styles.date}>{data.date}</h3>
+      <p style={styles.content}>{data.content}</p>
     </div>
   );
 };
