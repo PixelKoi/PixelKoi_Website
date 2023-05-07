8e35bb705ad94153a4deea8dd627e63d00e03a33
Jonatahn Bajada <jbajada@ryerson.ca>
2023-05-07 18:57:35 -0400

    update css

--- client/src/Pages/Case Study/component/CaseStudyHeader.tsx
+++ client/src/Pages/Case Study/component/CaseStudyHeader.tsx
@@ -5,6 +5,7 @@ interface CaseStudyHeaderProps {
 	img: string;
 	client: string;
 	releaseDate: string;
+	caseStudy: string;
 }
 
 const CaseStudyHeader = (props: CaseStudyHeaderProps) => {
@@ -13,7 +14,7 @@ const CaseStudyHeader = (props: CaseStudyHeaderProps) => {
 		return catArray.map((item) => {
 			return (
 				<div className={styles.catBorder} key={item}>
-					<p>{item}</p>
+					<p style={{ color: "#fff" }}>{item}</p>
 				</div>
 			);
 		});
@@ -26,19 +27,19 @@ const CaseStudyHeader = (props: CaseStudyHeaderProps) => {
 					<div className={styles.innerHeaderDescriptionContainer}>
 						<div className={styles.divideColumns}>
 							<div className={styles.colTitle}>
-								<p>Client</p>
+								<p>CLIENT</p>
 							</div>
 							<h5>{props.client}</h5>
 						</div>
 						<div className={styles.divideColumns}>
 							<div className={styles.colTitle}>
-								<p>Category</p>
+								<p>CATEGORY</p>
 							</div>
 							<div className={styles.catGroup}>{returnCat()}</div>
 						</div>
 						<div className={styles.divideColumns}>
 							<div className={styles.colTitle}>
-								<p>Release</p>
+								<p>RELEASE</p>
 							</div>
 							<h5>{props.releaseDate}</h5>
 						</div>
@@ -46,8 +47,8 @@ const CaseStudyHeader = (props: CaseStudyHeaderProps) => {
 				</div>
 
 				<div className={styles.caseTitleContainer}>
-					<h6>CASE STUDY</h6>
-					<h1>HYPE OVER NIGHT</h1>
+					<p>CASE STUDY</p>
+					<h1>{props.client}</h1>
 					<hr className={styles.line} />
 				</div>
 			</div>
