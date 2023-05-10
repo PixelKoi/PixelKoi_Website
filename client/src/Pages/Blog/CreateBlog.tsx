1742bc3b96acaf6d8a0c24d76188e18e13d3b282
Garabed96 <garonazarian09@gmail.com>
2023-05-10 22:17:30 +0700

    CreateBlog.tsx template, tailwindcss added.

--- client/src/Pages/Blog/CreateBlog.tsx
+++ client/src/Pages/Blog/CreateBlog.tsx
@@ -1,11 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-const CreateBlog = () => {
+function CreateBlog() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can add code to submit the blog post to your backend or save it to state
+  };
+
   return (
-    <div>
-      <p>Create Blog page</p>
+    <div className="p-5">
+      <h1>Create a Blog Post</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="content">Content:</label>
+          <textarea
+            id="content"
+            value={content}
+            onChange={handleContentChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
-};
+}
 
 export default CreateBlog;
