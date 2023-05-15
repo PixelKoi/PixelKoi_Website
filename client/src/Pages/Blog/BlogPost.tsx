26cd0f27a9d186d4acbc374755dc655d7f8029de
Garabed96 <garonazarian09@gmail.com>
2023-05-15 19:21:11 +0700

    BlogPost styling re-implemented

--- client/src/Pages/Blog/BlogPost.tsx
+++ client/src/Pages/Blog/BlogPost.tsx
@@ -2,12 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_API_KEY, SUPABASE_URL } from "../../../config";
+import { useNavigate } from "react-router-dom";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+
+// Rest of the imports
 
 const BlogPost = () => {
   const { slug } = useParams<{ slug: string }>();
   const [blog, setBlog] = useState<Object | null>(null);
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     console.log("SLUG", slug);
@@ -25,6 +30,13 @@ const BlogPost = () => {
         if (data) {
           console.log("DATABLOGPOST", data);
           setBlog(data[0]);
+
+          for (let i = 0; i < data[0].Images.length; i++) {
+            if (data[0].Images[i].image_url) {
+              setImageUrl(`/${data[0].Images[i].image_url}`);
+              break;
+            }
+          }
         }
       } catch (error) {
         console.error(error);
@@ -39,11 +51,103 @@ const BlogPost = () => {
   }
 
   return (
-    <div>
-      <h1>{blog.title}</h1>
-      <p>{blog.content}</p>
+    <div style={styles.mainContainer}>
+      <div style={styles.container}>
+        <button style={styles.button} onClick={() => navigate(-1)}>
+          Back to All Blogs
+        </button>
+        {imageUrl && (
+          <img src={imageUrl} alt="blog post" style={styles.image} />
+        )}
+        {blog && (
+          <>
+            <h1 style={styles.title}>{blog.title}</h1>
+            <h3 style={styles.author}>By {blog.author}</h3>
+            <h3 style={styles.date}>{blog.date}</h3>
+            <p style={styles.content}>{blog.content}</p>{" "}
+          </>
+        )}
+      </div>
     </div>
   );
+};
+
+const styles = {
+  mainContainer: {},
+  container: {
+    maxWidth: "800px",
+    margin: "10rem auto",
+    padding: "20px",
+    backgroundColor: "#f9f9f9",
+    boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.1)",
+    borderRadius: "5px",
+    fontFamily: "Arial, sans-serif",
+  },
+  title: {
+    padding: "0 1rem",
+    fontSize: "32px",
+    fontWeight: "bold",
+    marginBottom: "10px",
+    color: "#333",
+  },
+  author: {
+    padding: "0 1rem",
+    fontSize: "18px",
+    fontWeight: "bold",
+    marginBottom: "5px",
+    color: "#666",
+  },
+  date: {
+    padding: "0 1rem",
+    fontSize: "16px",
+    fontWeight: "normal",
+    marginBottom: "20px",
+    color: "#999",
+  },
+  content: {
+    padding: "0 1rem",
+    fontSize: "18px",
+    lineHeight: "28px",
+  },
+  imageContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center", // add this line to center vertically
+  },
+  image: {
+    padding: "1rem",
+    maxWidth: "100%",
+    height: "auto",
+    width: "750px",
+    maxHeight: "500px",
+  },
+  button: {
+    padding: "0.75rem 1.25rem",
+    fontSize: "1rem",
+    lineHeight: "1.5",
+    borderRadius: "0.25rem",
+    backgroundColor: "#007bff",
+    color: "#fff",
+    border: "none",
+    boxShadow: "0 0.5rem 1rem rgba(0, 0, 0, 0.15)",
+    cursor: "pointer",
+    transition: "all 0.15s ease-in-out",
+    marginTop: "1rem",
+  },
+  pre: {
+    backgroundColor: "#000",
+    padding: "16px",
+    borderRadius: "4px",
+    margin: "0",
+  },
+  code: {
+    display: "block",
+    whiteSpace: "pre-wrap",
+    fontFamily: "monospace",
+    fontSize: "14px",
+    lineHeight: "1.5",
+    color: "#fff",
+  },
 };
 
 export default BlogPost;
