0a86aecc21f4741d8eff9538631c01ab61555111
Garabed96 <garonazarian09@gmail.com>
2023-05-16 22:57:13 +0700

    login(auth) -> create post

--- client/src/components/Account/Account.tsx
+++ client/src/components/Account/Account.tsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { supabase } from "../../../config";
+import { Link } from "react-router-dom";
 
 export default function Account({ session }) {
   const [loading, setLoading] = useState(true);
   const [username, setUsername] = useState(null);
+  const [email, setEmail] = useState(null);
   const [website, setWebsite] = useState(null);
   const [avatar_url, setAvatarUrl] = useState(null);
 
@@ -14,16 +16,19 @@ export default function Account({ session }) {
 
       let { data, error } = await supabase
         .from("profiles")
-        .select(`username, website, avatar_url`)
+        .select(`username, website, avatar_url, email`)
         .eq("id", user.id)
         .single();
 
       if (error) {
         console.warn(error);
       } else if (data) {
+        console.log("GETTING DATA", data);
+        console.log("EMAIL", session.user.email);
         setUsername(data.username);
         setWebsite(data.website);
         setAvatarUrl(data.avatar_url);
+        setEmail(session.user.email);
       }
 
       setLoading(false);
@@ -42,6 +47,7 @@ export default function Account({ session }) {
       id: user.id,
       username,
       website,
+      email,
       avatar_url,
       updated_at: new Date(),
     };
@@ -55,50 +61,58 @@ export default function Account({ session }) {
   }
 
   return (
-    <form onSubmit={updateProfile} className="form-widget">
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session.user.email} disabled />
-      </div>
-      <div>
-        <label htmlFor="username">Name</label>
-        <input
-          id="username"
-          type="text"
-          required
-          value={username || ""}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="website">Website</label>
-        <input
-          id="website"
-          type="url"
-          value={website || ""}
-          onChange={(e) => setWebsite(e.target.value)}
-        />
-      </div>
+    <div>
+      <form onSubmit={updateProfile} className="form-widget">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text" value={session.user.email} disabled />
+        </div>
+        <div>
+          <label htmlFor="username">Name</label>
+          <input
+            id="username"
+            type="text"
+            required
+            value={username || ""}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="website">Website</label>
+          <input
+            id="website"
+            type="url"
+            value={website || ""}
+            onChange={(e) => setWebsite(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button
+            className="button block primary"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? "Loading ..." : "Update"}
+          </button>
+        </div>
+
+        <div>
+          <button
+            className="button block"
+            type="button"
+            onClick={() => supabase.auth.signOut()}
+          >
+            Sign Out
+          </button>
+        </div>
+      </form>
 
       <div>
-        <button
-          className="button block primary"
-          type="submit"
-          disabled={loading}
-        >
-          {loading ? "Loading ..." : "Update"}
+        <button className="flex mt-5">
+          <Link to="/createblog"> Create Post</Link>
         </button>
       </div>
-
-      <div>
-        <button
-          className="button block"
-          type="button"
-          onClick={() => supabase.auth.signOut()}
-        >
-          Sign Out
-        </button>
-      </div>
-    </form>
+    </div>
   );
 }
