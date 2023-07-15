9a5c1d46a39f74aab874363f646f4638f57266c5
Garabed96 <garonazarian09@gmail.com>
2023-07-15 07:46:37 +0700

    BlogPost.tsx styling fixes

--- client/src/Pages/Blog/BlogPost.tsx
+++ client/src/Pages/Blog/BlogPost.tsx
@@ -46,70 +46,87 @@ const BlogPost = () => {
   }, [slug]);
 
   if (!blog) {
-    return <div>Loading</div>;
+    return <div className="items-center text-center">Loading</div>;
   }
 
   return (
     <div style={styles.mainContainer}>
       <Nav />
       <div style={styles.container}>
-        <div className="flex justify-end">
-          <button
-            className="flex"
-            style={styles.button}
-            onClick={() => navigate(-1)}
-          >
-            Back to All Blogs
-          </button>
-        </div>
-
         {imageUrl && (
-          <img src={imageUrl} alt="blog post" style={styles.image} />
+            <>
+              <h1 className="text-center" style={styles.title}>{blog.title}</h1>
+              <h3 className="text-center" style={styles.author}>By {blog.author}</h3>
+              <div className="flexRow"> {/* Apply flexRow class here */}
+                <h3 className="text-center flex" style={styles.date}>
+                  {new Date(blog.date).toLocaleDateString(undefined, {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric"
+                  })}
+                </h3>
+                <div className="text-center op">5 minute read</div>
+              </div>
+              <div className="py-5" style={styles.imageContainer}>
+                <img  src={imageUrl} alt="blog post" style={styles.image} />
+              </div>
+
+            </>
+
         )}
         {blog && (
           <>
-            <h1 style={styles.title}>{blog.title}</h1>
-            <h3 style={styles.author}>By {blog.author}</h3>
-            <h3 style={styles.date}>{blog.date}</h3>
+
             <p style={styles.content}>{blog.content}</p>{" "}
           </>
         )}
+        <div className="flex text-center rounded my-4">
+          <a
+              className="flex items-center"
+              style={{ ...styles.button, textDecoration: "none" }}
+              onClick={() => navigate(-1)}
+          >
+            <span className="mr-2">&#8592;</span>
+            View all posts
+          </a>
+        </div>
       </div>
     </div>
   );
 };
 
 const styles = {
-  mainContainer: {},
+  mainContainer: {
+    height: "120vw"
+  },
   container: {
-    maxWidth: "800px",
+    maxWidth: "1000px",
     margin: "10rem auto",
     padding: "20px",
-    backgroundColor: "#f9f9f9",
-    boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.1)",
+    // backgroundColor: "#f9f9f9",
+    // boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.1)",
     borderRadius: "5px",
-    fontFamily: "Arial, sans-serif",
+    fontFamily: "Inter, sans-serif", // Fixed the syntax error here
   },
   title: {
-    padding: "0 1rem",
-    fontSize: "32px",
-    fontWeight: "bold",
+    padding: "2rem 1rem",
+    fontSize: "36px",
     marginBottom: "10px",
-    color: "#333",
   },
   author: {
     padding: "0 1rem",
     fontSize: "18px",
     fontWeight: "bold",
     marginBottom: "5px",
-    color: "#666",
+    color: "#FFFFFFl",
   },
   date: {
     padding: "0 1rem",
     fontSize: "16px",
     fontWeight: "normal",
     marginBottom: "20px",
-    color: "#999",
+    color: "#FFFFFF",
+    opacity: 0.6,
   },
   content: {
     padding: "0 1rem",
@@ -125,15 +142,12 @@ const styles = {
     padding: "1rem",
     maxWidth: "100%",
     height: "auto",
-    width: "750px",
-    maxHeight: "500px",
+    width: "1000px",
+    maxHeight: "600px",
+    margin: "0 auto", // Add this line to center the image horizontally
   },
+
   button: {
-    padding: "0.75rem 1.25rem",
-    borderRadius: "0.25rem",
-    backgroundColor: "#007bff",
-    color: "#fff",
-    boxShadow: "0 0.5rem 1rem rgba(0, 0, 0, 0.15)",
     cursor: "pointer",
     transition: "all 0.15s ease-in-out",
     margin: "1rem",
